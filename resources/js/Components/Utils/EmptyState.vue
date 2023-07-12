ebe895cf73c5ff107c6a5ea3b81e8ed26d8030e1
Raul Perusquia <raul@inikoo.com>
2023-07-12 11:13:50 +0800

    move EmptyState component 📂

---
+++ resources/js/Components/Utils/EmptyState.vue
@@ -0,0 +1,73 @@
+<!--
+  - Author: Raul Perusquia <raul@inikoo.com>
+  - Created: Wed, 12 Jul 2023 10:54:25 Malaysia Time, Kuala Lumpur, Malaysia
+  - Copyright (c) 2023, Raul A Perusquia Flores
+  -->
+
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
+import Button from "@/Components/Elements/Buttons/Button.vue"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCactus, faIslandTropical, faSkullCow, faFish } from "@/../private/pro-light-svg-icons"
+import { faPlus } from "@/../private/pro-regular-svg-icons"
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faPlus, faCactus, faIslandTropical, faSkullCow, faFish)
+import { trans } from 'laravel-vue-i18n'
+
+const props = defineProps<{
+    data: {
+        action: {
+            label: string
+            route: {
+                name: string
+                parameters: []
+            }
+            style: string
+            tooltip: string
+            type: string
+        }
+        description: string
+        title: string
+    }
+}>()
+
+const randomIcon = [
+    {
+        secondIcon: ['fal', 'fa-cactus'],
+        firstIcon: ['fal', 'fa-skull-cow'],
+    },
+    {
+        firstIcon: ['fal', 'fa-island-tropical'],
+        secondIcon: ['fal', 'fa-fish'],
+    },
+]
+
+const randomIndex = Math.floor(Math.random() * randomIcon.length)
+</script>
+
+<template>
+    <div class="text-center border-gray-200 pt-14">
+
+        <div class="mb-6">
+            <FontAwesomeIcon :icon="randomIcon[randomIndex].secondIcon" class="mx-auto h-9 text-gray-200" aria-hidden="true" />
+            <FontAwesomeIcon :icon="randomIcon[randomIndex].firstIcon" class="mx-7 h-12 w-12 text-gray-300" aria-hidden="true" />
+            <FontAwesomeIcon :icon="randomIcon[randomIndex].secondIcon" class="mx-auto h-8  text-gray-200" aria-hidden="true" />
+        </div>
+
+        <h3 class="font-logo text-lg font-bold text-gray-800">{{ data.title ?? trans('No records found') }}</h3>
+        <p v-if="data.description" class="text-sm mt-2 text-gray-500 mb-4">{{ data.description }}</p>
+        <Link v-if="data.action" :href="route(data.action.route.name, data.action.route.parameters)" class="">
+            <Button size="xs" :style="data.action.style"
+                class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm capitalize hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <FontAwesomeIcon icon="far fa-plus" class="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                {{ trans(data.action.tooltip) }}
+            </Button>
+
+            <!-- <Button size="xs" :type="action.style"
+                class="capitalize inline-flex items-center rounded-md border text-sm font-medium shadow-sm gap-x-2">
+                <FontAwesomeIcon v-if="getActionIcon(action)" :icon="getActionIcon(action)" class="" aria-hidden="true" />
+                {{ getActionLabel(action) }}
+            </Button> -->
+        </Link>
+    </div>
+</template>
