4f38df88066d93b27307a02537158a0cc5279120
artha <artha@aw-advantage.com>
2023-07-12 15:15:19 +0800

    PKA-968 Fix request logs slow

--- app/Http/Middleware/LogUserRequestMiddleware.php
+++ app/Http/Middleware/LogUserRequestMiddleware.php
@@ -17,7 +17,7 @@ class LogUserRequestMiddleware
         /* @var \App\Models\Auth\User $user */
         $user = $request->user();
 
-        if (!app()->runningUnitTests() && $user && env('ENABLE_LOGS')) {
+        if (!app()->runningUnitTests() && $user && env('USER_REQUEST_LOGGING')) {
             LogUserRequest::run(
                 now(),
                 [
