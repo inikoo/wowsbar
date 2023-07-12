0a25b8eb33450dec57f0bbbd442986c83b724332
artha <artha@aw-advantage.com>
2023-07-12 11:44:44 +0800

    PKA-964 Create scout search laravel

---
+++ app/Models/Search/UniversalSearch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Search;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UniversalSearch extends Model
+{
+    use HasFactory;
+}
