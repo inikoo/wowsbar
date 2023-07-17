d6e835da3fda32659b934808c4a463f904df458a
artha <artha@aw-advantage.com>
2023-07-17 15:45:49 +0800

    PKA-995 Modify universal search

--- app/Actions/Portfolio/ContentBlock/DeleteContentBlock.php
+++ app/Actions/Portfolio/ContentBlock/DeleteContentBlock.php
@@ -7,6 +7,7 @@
 
 namespace App\Actions\Portfolio\ContentBlock;
 
+use App\Actions\Portfolio\ContentBlock\Hydrators\ContentBlockHydrateUniversalSearch;
 use App\Models\Portfolio\ContentBlock;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
