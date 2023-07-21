3249f7317703b641e2f03ebb870010810bff5951
aqordeon <aqordivika@yahoo.co.id>
2023-07-21 14:37:28 +0800

    PKA-1004 Banner: Add headline, footer

--- resources/js/Components/Banner.vue
+++ resources/js/Components/Banner.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // Import Swiper Vue.js components
-import { defineAsyncComponent } from 'vue'
+import { ref } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Link } from "@inertiajs/vue3"
 import { Autoplay, Pagination, Navigation } from 'swiper/modules'
@@ -47,13 +47,64 @@ const getImageUrl = (name: string) => {
     return new URL(`@/../../../art/banner/` + name,import.meta.url).href
 }
 
+const dummyAbc = ref({
+                "delay": 5000,
+                "slides": [
+                    {
+                        "id": 1,
+                        "imageSrc": "incentives-07-hero.jpg",
+                        "imageAlt": "Front of men's Basic Tee in sienna",
+                        "link": {
+                            "label": "Open",
+                            "target": "#"
+                        },
+                        "text" : {
+                            "title": "Welcome to the jungle",
+                            "subtitle": "Jungle is the best place to go and healing with your family",
+                        }
+                    },
+                    {
+                        "id": 2,
+                        "imageSrc": "product-page-01-featured-product-shot.jpg",
+                        "imageAlt": "Lorem ipsum dolor sit amet consectetur.",
+                        "text" : {
+                            "position": "topleft",
+                            "title": "Welcome to the jungle",
+                            "subtitle": "Jungle is the best place to go and healing with your family",
+                        },
+                        "footer": {
+                            "position": "right",
+                            "label": "The banner officially disclaimed by XYZ Company."
+                        }
+                    },
+                    {
+                        "id": 3,
+                        "imageSrc": "product-page-03-product-01.jpg",
+                        "imageAlt": "Lorem ipsum dolor Basic Tee in sienna",
+                        "link": {
+                            "label": "Browse Promo",
+                            "target": "#"
+                        },
+                        "text" : {
+                            "title": "Welcome to the jungle",
+                            "subtitle": "Jungle is the best place to go and healing with your family",
+                        }
+                    },
+                    {
+                        "id": 4,
+                        "imageSrc": "product-page-05-product-01.jpg",
+                        "imageAlt": "Continuous infinite slider"
+                    }
+                ]
+        })
+
 </script>
 
 <template>
     <div class="w-full aspect-[16/4] overflow-hidden">
         <Swiper :spaceBetween="-1" :slidesPerView="1" :centeredSlides="true" :loop="true"
             :autoplay="{
-                delay: data.data.delay,
+                delay: 1000000,
                 disableOnInteraction: false,
             }"
             :pagination="{
@@ -61,8 +112,22 @@ const getImageUrl = (name: string) => {
             }"
             :navigation="false" :modules="[Autoplay, Pagination, Navigation]" class="mySwiper"
         >
-            <SwiperSlide v-for="slide in $props.data.data.slides" :key="slide.id">
+            <SwiperSlide v-for="slide in dummyAbc.slides" :key="slide.id">
                 <img :src="generateThumbnail(slide)" :alt="slide.imageAlt">
+                <div class="absolute space-y-2" :class="{
+                    'top-5 left-5 text-left': slide.text?.position == 'topleft',
+                    'top-5 right-5 text-right': slide.text?.position == 'topright',
+                    'bottom-5 left-5 text-left': slide.text?.position == 'bottomleft',
+                    'bottom-5 right-5 text-right': slide.text?.position == 'bottomright',
+                }"
+                >
+                    <div class="text-gray-100 drop-shadow-md text-5xl font-bold">{{ slide.text?.title }}</div>
+                    <div class="text-gray-300 drop-shadow text-lg italic tracking-widest">{{ slide.text?.subtitle }}</div>
+                </div>
+                <div v-if="slide.footer?.label" class="absolute text-gray-400 text-xs bottom-5"
+                    :class="{'left-5': slide.footer?.position == 'left', 'right-5': slide.footer?.position == 'right'}">
+                    {{ slide.footer.label }}
+                </div>
                 <Link v-if="slide.link" :href="slide.link.target"
                     class="bg-gray-800/40 text-gray-100 border border-gray-50/50 absolute bottom-6 right-11 rounded px-3 py-1 hover:bg-gray-900/60">
                     {{ slide.link.label }}
