584b9f149fef4351cc0a6cb4c660723b4c5b5605
aqordeon <aqordivika@yahoo.co.id>
2023-07-21 11:54:40 +0800

    Input Avatar: input only accept image, cleanup code

--- resources/js/Components/Forms/Fields/Avatar.vue
+++ resources/js/Components/Forms/Fields/Avatar.vue
@@ -1,9 +1,8 @@
 <script setup>
 import { trans } from "laravel-vue-i18n"
-import { ref } from "vue";
-import { watch } from "vue";
-const props = defineProps(['form', 'fieldName', 'options'])
+import { ref } from "vue"
 
+const props = defineProps(['form', 'fieldName', 'options'])
 const temporaryAvatar = ref(props.form.avatar)
 
 const avatarUploaded = (file) => {
@@ -12,20 +11,14 @@ const avatarUploaded = (file) => {
     reader.readAsDataURL(file)
     reader.onload = (e) => {
         temporaryAvatar.value = e.target.result
-        console.log(e.target.result)
-        console.log(temporaryAvatar)
-        //props.profile.avatar = e.target.result
     }
 }
-
 
 </script>
 
 <template>
     <div class=" w-fit">
-        <p class="text-sm font-medium text-gray-700" aria-hidden="true">
-            <!-- {{ pageBody.layout.profile.fields["avatar"].label }} -->
-        </p>
+        <!-- Avatar Button: Small view -->
         <div class="mt-1 lg:hidden">
             <div class="flex items-center">
                 <div class="inline-block h-12 w-12 flex-shrink-0 overflow-hidden rounded-full" aria-hidden="true">
@@ -34,35 +27,31 @@ const avatarUploaded = (file) => {
                 <div class="ml-5 rounded-md shadow-sm">
                     <div
                         class="group relative flex items-center justify-center rounded-md border border-gray-300 py-2 px-3 focus-within:ring-2 focus-within:ring-sky-500 focus-within:ring-offset-2 hover:bg-gray-50">
-                        <label for="mobile-user-photo"
-                               class="pointer-events-none relative text-sm font-medium leading-4 text-gray-700">
+                        <label for="input-avatar-small"
+                            class="pointer-events-none relative text-sm font-medium leading-4 text-gray-700">
                             <span>{{ trans("Change") }}</span>
-                            <span class="sr-only">
-                                <!-- {{ pageBody.layout.profile.fields["avatar"].info }} -->
-                            </span>
                         </label>
-                        <input id="mobile-user-photo" name="user-photo" type="file"
-                               @input="avatarUploaded($event.target.files[0])"
-                               class="absolute h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
+                        <input id="input-avatar-small" name="user-photo" type="file"
+                            @input="avatarUploaded($event.target.files[0])"
+                            class="absolute h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- Avatar Button: Large view -->
         <div class="relative hidden overflow-hidden rounded-full lg:block">
             <img class="relative h-40 w-40 rounded-full" :src="temporaryAvatar" alt="" />
-            <label id="desktop-user-photo-mask" for="desktop-user-photo"
-                   class="absolute inset-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 text-sm font-medium text-white opacity-0 hover:opacity-100">
+            <label id="input-avatar-large-mask" for="input-avatar-large"
+                class="absolute inset-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 text-sm font-medium text-white opacity-0 hover:opacity-100">
                 <span>{{ trans("Change") }}</span>
-                <span class="sr-only">
-                    <!-- {{ pageBody.layout.profile.fields["photo"].info }} -->
-                </span>
-                <input type="file" @input="avatarUploaded($event.target.files[0])" id="desktop-user-photo" name="user-photo"
-                       class="absolute inset-0 h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
+                <input type="file" @input="avatarUploaded($event.target.files[0])" id="input-avatar-large" name="input-avatar-large" accept="image/*"
+                    class="absolute inset-0 h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
             </label>
         </div>
-        <!-- <div v-if="profileForm.errors.avatar" class="text-red-700">
-            {{ profileForm.errors.avatar }}
-        </div> -->
+        <div v-if="props.form.errors.avatar" class="text-red-700">
+            {{ props.form.errors.avatar }}
+        </div>
     </div>
 </template>
 
