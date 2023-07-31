cec9cddb2a64ddaaeeb9d9c2303ea1b115f147dc
aqordeon <aqordivika@yahoo.co.id>
2023-07-31 08:29:22 +0800

    Fix error UI on SlideWorkshop

--- resources/js/Components/Workshop/SlideWorkshop.vue
+++ resources/js/Components/Workshop/SlideWorkshop.vue
@@ -61,7 +61,7 @@ const setCurrent=(key)=>{
     <div class="divide-y divide-gray-200 lg:grid grid-flow-col lg:grid-cols-12 lg:divide-y-0 lg:divide-x overflow-auto h-full">
 
         <!-- Left Tab: Navigation -->
-        <aside class="py-0 lg:col-span-3 lg:h-full">
+        <aside class="py-0 lg:col-span-3 lg:h-full border-2 border-red-500">
             <nav role="navigation" class="space-y-1">
                 <ul>
                     <li v-for="(item, key) in blueprint" @click="setCurrent(key)" :class="[
@@ -78,27 +78,25 @@ const setCurrent=(key)=>{
         </aside>
 
         <!-- Content of forms -->
-        <div class="px-4 sm:px-6 md:px-4 pt-6 xl:pt-0 col-span-9 flex justify-center">
-            <div class="flex flex-col">
-                <div v-for="(fieldData, index ) in blueprint[current].fields" :key="index" class="">
-                    <dl class="pb-4 sm:pb-5 sm:gap-4 w-screen max-w-xl">
-                        <!-- Title -->
-                        <dt v-if="fieldData.name != 'image_source'" class="text-sm font-medium text-gray-500 capitalize">
-                            <div class="inline-flex items-start leading-none">
-                                <span>{{ fieldData.label }}</span>
-                            </div>
-                        </dt>
+        <div class="px-4 sm:px-6 md:px-4 pt-6 xl:pt-0 col-span-9 flex flex-grow justify-center">
+            <div class="flex flex-col w-full border-2 border-red-500">
+                <dl v-for="(fieldData, index ) in blueprint[current].fields" :key="index" class="pb-4 sm:pb-5 sm:gap-4 w-full">
+                    <!-- Title -->
+                    <dt v-if="fieldData.name != 'image_source' && fieldData.label" class="text-sm font-medium text-gray-500 capitalize">
+                        <div class="inline-flex items-start leading-none">
+                            <span>{{ fieldData.label }}</span>
+                        </div>
+                    </dt>
 
-                        <!-- Fields -->
-                        <dd class="flex text-sm text-gray-700 sm:mt-0">
-                            <div class="relative flex-grow">
-                                <component :is="getComponent(fieldData['type'])" :data="currentComponentBeenEdited"
-                                    :fieldName="fieldData.name" :fieldData="fieldData" :key="index" :counter="false" :common="common">
-                                </component>
-                            </div>
-                        </dd>
-                    </dl>
-                </div>
+                    <!-- Fields -->
+                    <dd class="flex text-sm text-gray-700 sm:mt-0 w-full border-yellow-500">
+                        <div class="relative flex-grow">
+                            <component :is="getComponent(fieldData['type'])" :data="currentComponentBeenEdited"
+                                :fieldName="fieldData.name" :fieldData="fieldData" :key="index" :counter="false" :common="common">
+                            </component>
+                        </div>
+                    </dd>
+                </dl>
             </div>
         </div>
     </div>
