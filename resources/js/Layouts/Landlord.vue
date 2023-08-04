631f0a4857321b0ac2cf64ed278c016adb2e45e7
aqordeon <aqordivika@yahoo.co.id>
2023-08-04 16:58:41 +0800

    Pricing: add layout Landlord

--- resources/js/Layouts/Landlord.vue
+++ resources/js/Layouts/Landlord.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="relative ">
-        <div class="bg-gradient-to-b from-gray-100/50 to-gray-100/0 w-screen fixed top-0 z-10 px-8 py-1 flex justify-between items-center">
+        <div class="bg-gradient-to-b from-gray-100/50 to-gray-100/0 w-screen fixed top-3 z-10 px-8 py-1 flex justify-between items-center">
             <Link :href="route('landlord.welcome')">
                 <img class="h-6 select-none" src="@/../art/logo/png/1.png" alt="Wowsbar" />
             </Link>
-            <div class="flex justify-end gap-x-4 text-sm">
-                <Link :href="route('landlord.login')" class="bg-gray-500 text-gray-100 py-0.5 flex place-items-center cursor-pointer px-4 rounded hover:bg-gray-600/90 active:bg-gray-600 select-none">
+            <div class="flex justify-end gap-x-4 text-sm font-medium">
+                <Link :href="route('landlord.login')" class="bg-gray-500 text-gray-100 py-0.5 flex place-items-center cursor-pointer px-4 rounded-md hover:bg-gray-600/90 active:bg-gray-600 select-none">
                     Login
                 </Link>
-                <Link :href="route('landlord.register')" class="bg-gray-100/30 border-2 border-gray-500 cursor-pointer text-gray-600 py-0.5 flex place-items-center px-4 rounded hover:bg-gray-200/70 active:bg-gray-300 select-none">
+                <Link :href="route('landlord.register')" class="bg-gray-100/30 border-2 border-gray-500 cursor-pointer text-gray-600 py-0.5 flex place-items-center px-4 rounded-md hover:bg-gray-200/70 active:bg-gray-300 select-none">
                     Register
                 </Link>
             </div>
