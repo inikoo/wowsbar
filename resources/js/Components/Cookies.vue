2986ba1cfd9e05537d82e6357d4c757f83f22d76
aqordeon <aqordivika@yahoo.co.id>
2023-08-09 14:55:03 +0800

    PKA-1032 Component: Cookies Acceptation

--- resources/js/Components/Cookies.vue
+++ resources/js/Components/Cookies.vue
@@ -1,28 +1,103 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCookieBite } from '@/../private/pro-solid-svg-icons'
 import { faTimes } from '@/../private/pro-light-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { ref, Ref, computed } from 'vue'
 library.add(faCookieBite, faTimes)
+
+// make it True so the popup is disappear at first load
+const isCookieAccepted = ref(true)
+
+const setCookie = (cookieName: string, cookieValue: string, expDay: number) => {
+    const d = new Date()
+    d.setTime(d.getTime() + (expDay * 24 * 60 * 60 * 1000))
+    document.cookie = cookieName + "=" + cookieValue + ";expires=" + d.toUTCString() + ";path=/"
+}
+
+const getCookie = (cookieName: string) => {
+    let name = cookieName + "="
+    let decodedCookie = decodeURIComponent(document.cookie)
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length)
+        }
+    }
+    return ""
+}
+
+const checkCookie = (cookieName: string) => {
+    let cookieValue = getCookie(cookieName)
+    // console.log(cookieValue)
+    if (cookieValue != "") {
+        // alert("Welcome again " + cookieValue)
+        return true
+    } else {
+        // cookieValue = prompt('ddd')
+        // if (cookieValue != "" && cookieValue != null) {
+        //     setCookie("username", cookieValue, 30)
+        // }
+        return false
+    }
+}
+
+const acceptAllCokies = () => {
+    setCookie("isCookieAccepted", 'true', 999999999999)
+    isCookieAccepted.value = true
+    // Add more function
+}
+
+const acceptNecessaryCokies = () => {
+    setCookie("isCookieAccepted", 'true', 999999999999)
+    isCookieAccepted.value = true
+    // Add more function below
+}
+
+const compCookieAccepted = computed(() => {
+    return checkCookie("isCookieAccepted")
+})
+
+onMounted(() => {
+    setTimeout(() => {
+        // if the popup is not setted before, The popup will appear after 5 seconds
+        isCookieAccepted.value = checkCookie("isCookieAccepted")
+    }, 5000);
+})
 </script>
 
 <template>
-    <div class="fixed bottom-4 right-4 bg-white rounded-lg shadow-md pt-8 pb-6 px-6 flex flex-col justify-start gap-y-2 ring-1 ring-gray-200">
-        <!-- <div @click="" class="absolute top-2 right-2 text-gray-400 w-8 h-8 grid place-items-center rounded-full cursor-pointer hover:text-gray-600">
-            <FontAwesomeIcon icon='fal fa-times' class='' aria-hidden='true' />
-        </div> -->
-        <div class="flex gap-x-1">
-            <FontAwesomeIcon icon='fas fa-cookie-bite' class='text-yellow-700' size="xl" aria-hidden='true' />
-            <div class="text-yellow-800 text-xl font-bold">Yet another cookie disclaimer!</div>
+    <Transition name="cookies">
+        <div v-if="!isCookieAccepted" class="fixed bottom-[3%] right-4 bg-white rounded-lg shadow-md pt-8 pb-6 px-6 flex flex-col justify-start gap-y-2 ring-1 ring-gray-200">
+            <!-- <div @click="" class="absolute top-2 right-2 text-gray-400 w-8 h-8 grid place-items-center rounded-full cursor-pointer hover:text-gray-600">
+                <FontAwesomeIcon icon='fal fa-times' class='' aria-hidden='true' />
+            </div> -->
+            <div class="flex gap-x-1">
+                <FontAwesomeIcon icon='fas fa-cookie-bite' class='text-yellow-700' size="xl" aria-hidden='true' />
+                <div class="text-yellow-800 text-xl font-bold">Yet another cookie disclaimer!</div>
+            </div>
+            <div class="text-sm text-gray-600">We use cookies to improve your experience.<br>By using Wowsbar you're agreeing to the collection<br>of data as describer in our Cookie Policy.</div>
+            <div class="flex gap-x-4 mt-3">
+                <div @click="acceptNecessaryCokies" class="py-1 px-4 ring-1 ring-gray-400 rounded-full cursor-pointer bg-white hover:bg-gray-100 active:bg-gray-200 select-none">Necessary cookies only</div>
+                <div @click="acceptAllCokies" class="py-1 px-4 text-gray-100 rounded-full cursor-pointer button-wowsbar">Accept all cookies</div>
+            </div>
         </div>
-        <div class="text-sm text-gray-600">We use cookies to improve your experience.<br>By using Wowsbar you're agreeing to the collection<br>of data as describer in our Cookie Policy.</div>
-        <div class="flex gap-x-4 mt-3">
-            <div class="py-1 px-4 ring-1 ring-gray-400 rounded-full cursor-pointer bg-white hover:bg-gray-100 active:bg-gray-200 select-none">Necessary cookies only</div>
-            <div class="py-1 px-4 text-gray-100 rounded-full cursor-pointer button-wowsbar">Accept all cookies</div>
-        </div>
-    </div>
+    </Transition>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.cookies-enter-active,
+.cookies-leave-active {
+    transition: all 0.2s ease-in-out
+}
 
+.cookies-enter-from,
+.cookies-leave-to {
+    transform: translateY(100%);
+}
 </style>
