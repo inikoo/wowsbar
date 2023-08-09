f77ea941026ec2375ca2c1e9c77655a18310ebc9
aqordeon <aqordivika@yahoo.co.id>
2023-08-09 16:43:41 +0800

    Sign in to Log in

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -49,7 +49,7 @@ const submit = () => {
         <div>
             <button type="submit"
                     class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                {{ trans('Sign in') }}
+                {{ trans('Login') }}
             </button>
         </div>
     </form>
