84776dae8a17052790926f4e53f0a0171bf2386d
Raul Perusquia <raul@inikoo.com>
2023-08-10 14:06:17 +0800

    small TLC 🔗🔪😋

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             CurrencySeeder::class,
             TimezoneSeeder::class,
             LanguageSeeder::class,
+            StockImageSeeder::class
         ]);
     }
 }
