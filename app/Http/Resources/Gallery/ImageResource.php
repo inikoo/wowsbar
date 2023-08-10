c044ef1bf3eeb9c2363d22909d37064e17ca7b5a
Raul Perusquia <raul@inikoo.com>
2023-08-10 16:14:56 +0800

    dont know what i am doing

--- app/Http/Resources/Gallery/ImageResource.php
+++ app/Http/Resources/Gallery/ImageResource.php
@@ -10,17 +10,21 @@ namespace App\Http\Resources\Gallery;
 use App\Actions\Helpers\Images\GetPictureSources;
 use App\Helpers\ImgProxy\Image;
 use App\Helpers\NaturalLanguage;
+use App\Http\Resources\HasSelfCall;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ImageResource extends JsonResource
 {
+    use HasSelfCall;
+
     public function toArray($request): array
     {
         /** @var \App\Models\Media\Media $media */
         $media = $this;
 
 
-        $image = (new Image())->make($media->getLocalImgProxyFilename())->resize(0, 48);
+        $image          = (new Image())->make($media->getLocalImgProxyFilename());
+        $imageThumbnail = $image->resize(0, 48);
 
 
         return [
@@ -29,7 +33,8 @@ class ImageResource extends JsonResource
             'name'      => $media->name,
             'mime_type' => $media->mime_type,
             'size'      => NaturalLanguage::make()->fileSize($media->size),
-            'thumbnail' => GetPictureSources::run($image)
+            'thumbnail' => GetPictureSources::run($imageThumbnail),
+            'source'    => GetPictureSources::run($image),
 
 
         ];
