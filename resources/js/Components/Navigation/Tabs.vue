8564407c91b85d6d83898ecd35f192c661b38b3c
aqordeon <aqordivika@yahoo.co.id>
2023-08-10 13:59:39 +0800

    Tabs: Animation on select and hover

--- resources/js/Components/Navigation/Tabs.vue
+++ resources/js/Components/Navigation/Tabs.vue
@@ -5,12 +5,12 @@
   -->
 
 <script setup lang="ts">
-import {ref} from "vue";
-import {capitalize} from "@/Composables/capitalize"
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import {faInfoCircle} from "@/../private/pro-solid-svg-icons"
-import {faRoad, faClock, faDatabase} from "@/../private/pro-light-svg-icons"
-import {library} from '@fortawesome/fontawesome-svg-core'
+import { ref } from "vue"
+import { capitalize } from "@/Composables/capitalize"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faInfoCircle } from "@/../private/pro-solid-svg-icons"
+import { faRoad, faClock, faDatabase } from "@/../private/pro-light-svg-icons"
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 library.add(faInfoCircle, faRoad, faClock, faDatabase)
 
@@ -27,7 +27,7 @@ const changeTab = (tabSlug: string) => {
     currentTab.value = tabSlug;
 }
 
-const tabIconClass = (current, type, align, extraClass) => {
+const tabIconClass = (current: string, type: string, align: string, extraClass: string) => {
     let iconClass = '-ml-0.5 h-5 w-5   ' + extraClass;
     iconClass += current ? '' : 'text-gray-400 dark:text-gray-500 group-hover:text-gray-500 ';
     iconClass += (type == 'icon' && align == 'right') ? 'ml-2 ' : 'mr-2 '
@@ -51,21 +51,26 @@ const tabIconClass = (current, type, align, extraClass) => {
                 <!-- Left section -->
                 <nav class="-mb-px flex grow space-x-6 ml-4" aria-label="Tabs">
                     <template v-for="(tab, tabSlug) in navigation" :key="tabSlug">
-                        <button
-                            v-if="tab.align !== 'right'"
-                            @click="[$emit('update:tab', tabSlug), changeTab(tabSlug)]"
-                            :class="[tabSlug === currentTab ? 'border-orange-500 dark:border-gray-300 text-gray-600 dark:text-gray-400' : 'border-transparent text-gray-500 hover:text-gray-700 dark:hover:text-gray-400 hover:border-gray-300',
-                                'group inline-flex items-center py-2 px-1 border-b-2 font-medium text-sm']"
-                            :aria-current="tabSlug === currentTab ? 'page' : undefined">
-                            <FontAwesomeIcon v-if="tab.icon" :icon="tab.icon" :class="tabIconClass(tabSlug === currentTab, tab.type, tab.align, tab.iconClass ?? '')" aria-hidden="true"/>
-                            <span v-if="tab.type !== 'icon'" class="capitalize">{{ tab.title }}</span>
-                        </button>
+                        <div class="relative group">
+                            <button
+                                v-if="tab.align !== 'right'"
+                                @click="[$emit('update:tab', tabSlug), changeTab(tabSlug)]"
+                                :class="[
+                                    'group inline-flex items-center py-2 px-1 border-b-2 font-medium text-sm']"
+                                :aria-current="tabSlug === currentTab ? 'page' : undefined">
+                                <FontAwesomeIcon v-if="tab.icon" :icon="tab.icon" :class="tabIconClass(tabSlug === currentTab, tab.type, tab.align, tab.iconClass ?? '')" aria-hidden="true"/>
+                                <span v-if="tab.type !== 'icon'" class="capitalize">{{ tab.title }}</span>
+                            </button>
+                            <div class="absolute h-0.5 rounded-full -bottom-2 xl:bottom-0 left-[50%] translate-x-[-50%] mx-auto transition-all duration-200 ease-in-out"
+                                :class="[tabSlug === currentTab ? 'bg-orange-500 dark:bg-gray-300 w-full' : 'bg-gray-400 w-0 group-hover:w-3/6']"
+                            />
+                        </div>
                     </template>
                 </nav>
 
                 <!-- Right section -->
                 <nav class="flex flex-row-reverse mr-4" aria-label="Secondary Tabs">
-                    <template v-for="(tab,tabSlug) in navigation" :key="tabSlug">
+                    <template v-for="(tab, tabSlug) in navigation" :key="tabSlug">
                         <button
                             v-if="tab.align === 'right'"
                             @click="[$emit('update:tab', tabSlug), changeTab(tabSlug)]"
