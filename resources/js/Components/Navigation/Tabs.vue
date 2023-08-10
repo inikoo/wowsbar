a23047396d87b8d91c87e4d97d57025d937dcd48
aqordeon <aqordivika@yahoo.co.id>
2023-08-10 15:13:54 +0800

    Tabs: fix animation

--- resources/js/Components/Navigation/Tabs.vue
+++ resources/js/Components/Navigation/Tabs.vue
@@ -46,7 +46,7 @@ const tabIconClass = (current: string, type: string, align: string, extraClass: 
             </select>
         </div>
         <div class="hidden sm:block">
-            <div class="border-b border-gray-200 dark:border-gray-500 flex">
+            <div class="border-b border-gray-200 dark:border-gray-500 flex text-gray-500">
 
                 <!-- Left section -->
                 <nav class="-mb-px flex grow space-x-6 ml-4" aria-label="Tabs">
@@ -56,12 +56,12 @@ const tabIconClass = (current: string, type: string, align: string, extraClass: 
                                 v-if="tab.align !== 'right'"
                                 @click="[$emit('update:tab', tabSlug), changeTab(tabSlug)]"
                                 :class="[
-                                    'group inline-flex items-center py-2 px-1 border-b-2 font-medium text-sm']"
+                                    'group inline-flex items-center py-2 px-1 font-medium text-sm']"
                                 :aria-current="tabSlug === currentTab ? 'page' : undefined">
                                 <FontAwesomeIcon v-if="tab.icon" :icon="tab.icon" :class="tabIconClass(tabSlug === currentTab, tab.type, tab.align, tab.iconClass ?? '')" aria-hidden="true"/>
                                 <span v-if="tab.type !== 'icon'" class="capitalize">{{ tab.title }}</span>
                             </button>
-                            <div class="absolute h-0.5 rounded-full -bottom-2 xl:bottom-0 left-[50%] translate-x-[-50%] mx-auto transition-all duration-200 ease-in-out"
+                            <div class="absolute h-0.5 rounded-full bottom0 xl:bottom-0 left-[50%] translate-x-[-50%] mx-auto transition-all duration-200 ease-in-out"
                                 :class="[tabSlug === currentTab ? 'bg-orange-500 dark:bg-gray-300 w-full' : 'bg-gray-400 w-0 group-hover:w-3/6']"
                             />
                         </div>
@@ -71,15 +71,19 @@ const tabIconClass = (current: string, type: string, align: string, extraClass: 
                 <!-- Right section -->
                 <nav class="flex flex-row-reverse mr-4" aria-label="Secondary Tabs">
                     <template v-for="(tab, tabSlug) in navigation" :key="tabSlug">
-                        <button
-                            v-if="tab.align === 'right'"
-                            @click="[$emit('update:tab', tabSlug), changeTab(tabSlug)]"
-                            :class="[tabSlug === currentTab ? 'border-orange-500 dark:border-gray-300 text-gray-600 dark:text-gray-400' : 'border-transparent text-gray-500 hover:text-gray-700 dark:hover:text-gray-400 hover:border-gray-300',
-                                'group inline-flex justify-center items-center py-2 px-2 border-b-2 font-medium text-sm']"
-                            :aria-current="tabSlug === currentTab ? 'page' : undefined">
-                            <FontAwesomeIcon :title="capitalize(tab.title)" v-if="tab.icon" :icon="tab.icon" class="h-5 w-5" aria-hidden="true"/>
-                            <span v-if="tab.type!=='icon'" class="capitalize">{{ tab.title }}</span>
-                        </button>
+                        <div class="relative group">
+                            <button
+                                v-if="tab.align === 'right'"
+                                @click="[$emit('update:tab', tabSlug), changeTab(tabSlug)]"
+                                :class="['group inline-flex justify-center items-center py-2 px-2 font-medium text-sm']"
+                                :aria-current="tabSlug === currentTab ? 'page' : undefined">
+                                <FontAwesomeIcon :title="capitalize(tab.title)" v-if="tab.icon" :icon="tab.icon" class="h-5 w-5" aria-hidden="true"/>
+                                <span v-if="tab.type!=='icon'" class="capitalize">{{ tab.title }}</span>
+                            </button>
+                            <div class="absolute h-0.5 rounded-full -bottom-2 xl:bottom-0 left-[50%] translate-x-[-50%] mx-auto transition-all duration-200 ease-in-out"
+                                :class="[tabSlug === currentTab ? 'bg-orange-500 dark:bg-gray-300 w-full' : 'bg-gray-400 w-0 group-hover:w-3/6']"
+                            />
+                        </div>
                     </template>
                 </nav>
             </div>
