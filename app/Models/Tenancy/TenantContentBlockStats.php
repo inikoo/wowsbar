216681db6be7e599d6cbe3bd3b2d34d091721f05
artha <artha@aw-advantage.com>
2023-08-11 16:21:43 +0800

    PKA-1038 Create hydrators for banners

---
+++ app/Models/Tenancy/TenantContentBlockStats.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Tenancy;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TenantContentBlockStats extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
