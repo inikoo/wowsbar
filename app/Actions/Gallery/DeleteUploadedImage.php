6fe8732f1c7fbb8322851851cad73f1b998a01f9
artha <artha@aw-advantage.com>
2023-08-11 13:52:15 +0800

    PKA-1037 Create actions for state in content block

---
+++ app/Actions/Gallery/DeleteUploadedImage.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 01 Aug 2023 13:04:01 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Gallery;
+
+use App\Actions\Traits\WithActionUpdate;
+use App\Models\Media\Media;
+use App\Models\Portfolio\Website;
+use Illuminate\Http\RedirectResponse;
+use Lorisleiva\Actions\ActionRequest;
+
+class DeleteUploadedImage
+{
+    use WithActionUpdate;
+
+    private Website|null $website = null;
+
+    public function handle(Media $media): bool
+    {
+        return $media->delete();
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        return $request->user()->can("portfolio.edit");
+    }
+
+    public function htmlResponse(): RedirectResponse
+    {
+        return redirect()->route('portfolio.gallery');
+    }
+
+    public function asController(Media $media): bool
+    {
+        return $this->handle($media);
+    }
+}
