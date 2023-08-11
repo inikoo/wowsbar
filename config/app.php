80be17cdb9f2da13f04182b71b3793a088a1fc31
Raul Perusquia <raul@inikoo.com>
2023-08-11 15:25:13 +0800

    telescope + upgrade AWS 🔭

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\MacroServiceProvider::class,
         ElasticSearchServiceProvider::class
 
