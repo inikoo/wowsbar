1d39ea5406b276a0084538570e943d716bfa630c
aqordeon <aqordivika@yahoo.co.id>
2023-08-14 14:44:13 +0800

    TopBar: Restructure for Org, Public, Tenant

--- resources/js/Layouts/PublicApp.vue
+++ resources/js/Layouts/PublicApp.vue
@@ -6,16 +6,6 @@
 
 <script setup lang="ts">
 import { ref, watchEffect } from "vue";
-import {
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems
-} from "@headlessui/vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue"
-import Button from "@/Components/Elements/Buttons/Button.vue"
-import SearchBar from "@/Components/SearchBar.vue"
 import { usePage, router } from "@inertiajs/vue3"
 
 import { useLayoutStore } from "@/Stores/layout"
@@ -26,10 +16,8 @@ import AppRightSideBar from "@/Layouts/AppRightSideBar.vue"
 import AppTopBar from "@/Layouts/TopBar/AppTopBar.vue"
 import Breadcrumbs from "@/Components/Navigation/Breadcrumbs.vue"
 
-import { loadLanguageAsync, trans } from "laravel-vue-i18n"
-import { Link } from "@inertiajs/vue3"
+import { loadLanguageAsync } from "laravel-vue-i18n"
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { useAppearanceStore } from "@/Stores/appearance";
 
 import {
     faHome,
@@ -50,11 +38,11 @@ import {
     faGlobe,
     faLanguage
 } from "@/../private/pro-light-svg-icons"
-import { faSearch, faBell} from "@/../private/pro-regular-svg-icons"
-import { onMounted } from "vue";
-import {useFirestore} from "vuefire";
-import {useFirebaseStore} from "@/Stores/firebase";
-import AppPublicFooter from "@/Layouts/AppPublicFooter.vue";
+import { faSearch, faBell } from "@/../private/pro-regular-svg-icons"
+import { onMounted } from "vue"
+import { useFirestore } from "vuefire"
+import { useFirebaseStore } from "@/Stores/firebase"
+import AppPublicFooter from "@/Layouts/AppPublicFooter.vue"
 
 
 library.add(
@@ -188,36 +176,8 @@ router.on('navigate', () => {
 
 const layout = initialiseApp()
 const sidebarOpen = ref(false)
-const showSearchDialog = ref(false)
-const user = ref(usePage().props.auth.user)
 
-const changeColorMode = (mode: boolean | string) => {
-    // If browsers not support matchMedia
-    if (!window.matchMedia) {
-        return
-    }
 
-    let query: boolean | string = false
-
-    if (mode == "system") {
-        // If browsers prefers dark-mode then true
-        query = window.matchMedia('(prefers-color-scheme: dark)').matches
-    } else {
-        query = mode
-    }
-
-    if(query) {
-        document.documentElement.classList.add('dark')
-        localStorage.setItem('darkMode', `${query}`)
-    } else {
-        document.documentElement.classList.remove('dark')
-        localStorage.setItem('darkMode', `${query}`)
-    }
-}
-
-onMounted(() => {
-    useAppearanceStore().darkMode ? document.documentElement.classList.add('dark') : ''
-})
 
 </script>
 
@@ -226,135 +186,13 @@ onMounted(() => {
     <div class="relative min-h-full transition-all duration-200 ease-in-out"
         :class="[Object.values(layout.rightSidebar).some(value => value === true) ? 'mr-44' : 'mr-0']"
     >
-
         <!-- TopBar -->
-        <Disclosure as="nav" class=" fixed top-0 z-20 w-full bg-gray-50 text-gray-700 dark:bg-gray-800 dark:text-gray-200" v-slot="{ open }">
-            <div class="px-0">
-                <div class="flex h-11 lg:h-10 flex-shrink-0 border-b border-gray-200 dark:border-gray-500 ">
-                    <div class="flex flex-1">
-                        <div class="flex flex-1 lg:justify-between">
-                            <!-- Hamburger -->
-                            <button class="block md:hidden w-10 h-10 relative focus:outline-none bg-white" @click="sidebarOpen = !sidebarOpen">
-                                <span class="sr-only">Open sidebar</span>
-                                <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
-                                    <span aria-hidden="true" class="block absolute rounded-full h-0.5 w-5 bg-gray-900 transform transition duration-200 ease-in-out"
-                                        :class="{'rotate-45': sidebarOpen,' -translate-y-1.5': !sidebarOpen }"></span>
-                                    <span aria-hidden="true" class="block absolute rounded-full h-0.5 w-5 bg-gray-900 transform transition duration-100 ease-in-out" :class="{'opacity-0': sidebarOpen } "></span>
-                                    <span aria-hidden="true" class="block absolute rounded-full h-0.5 w-5 bg-gray-900 transform transition duration-200 ease-in-out"
-                                        :class="{'-rotate-45': sidebarOpen, ' translate-y-1.5': !sidebarOpen}"></span>
-                                </div>
-                            </button>
-                            <!-- Menu -->
-
-                            <AppTopBar :dashboardRoute="'public.dashboard.show'"   />
-
-                        </div>
-
-                        <!-- Avatar Group -->
-                        <div class="flex items-center mr-6 space-x-3">
-                            <div class="flex">
-                                <!-- <div class="cursor-pointer text-white bg-indigo-500 px-2 py-0.5 rounded-md select-none" @click="changeColorMode(true)">Dark mode: True</div>
-                                <div class="cursor-pointer text-white bg-indigo-500 px-2 py-0.5 rounded-md select-none" @click="changeColorMode(false)">Dark mode: False</div>
-                                <div class="cursor-pointer text-white bg-indigo-500 px-2 py-0.5 rounded-md select-none" @click="changeColorMode('system')">Dark mode: OS System</div> -->
-
-                                <!-- Button: Search -->
-                                <button @click="showSearchDialog = !showSearchDialog"
-                                        class="h-8 w-8 grid items-center justify-center rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500">
-                                    <span class="sr-only">{{ trans("Search") }}</span>
-                                    <font-awesome-icon aria-hidden="true" icon="fa-regular fa-search" size="lg" />
-                                    <SearchBar v-if="showSearchDialog" v-on:close="showSearchDialog = false" />
-                                </button>
-
-                                <!-- Button: Notifications -->
-                                <button type="button"
-                                        class="h-8 w-8 grid items-center justify-center rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500">
-                                    <span class="sr-only">{{ trans("View notifications") }}</span>
-                                    <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bell" size="lg" />
-                                </button>
-                            </div>
-
-                            <!-- Avatar Button -->
-                            <Menu as="div" class="relative">
-                                <MenuButton
-                                    class="flex max-w-xs items-center rounded-full bg-gray-100 text-sm focus:outline-none focus:ring-2 focus:ring-gray-500">
-                                    <span class="sr-only">{{ trans("Open user menu") }}</span>
-                                    <img v-if="layout.avatar_id" class="h-8 w-8 rounded-full"
-                                        :src="route('media.show',layout.avatar_id)"
-                                        alt="" />
-                                    <img v-if="!layout.avatar_id" class="h-8 w-8 rounded-full"
-                                         src="https://ui-avatars.com/api/?background=random"
-                                         alt="" />
-                                </MenuButton>
-
-                                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-                                            leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                                    <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none">
-                                        <div class="py-1">
-                                            <MenuItem v-slot="{ active,close }">
-                                                <Link as="ul" type="button" :href="route('profile.show')" @click="close"
-                                                    :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm cursor-pointer']">
-                                                    {{ trans("View profile") }}
-                                                </Link>
-                                            </MenuItem>
-
-                                        </div>
-
-                                        <div class="py-1">
-                                            <MenuItem v-slot="{ active }">
-                                                <Link as="ul" type="button" method="post" :href="route('logout')"
-                                                    :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm cursor-pointer']">Logout
-                                                </Link>
-                                            </MenuItem>
-                                        </div>
-                                    </MenuItems>
-                                </transition>
-                            </Menu>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-
-            <!-- <DisclosurePanel class="md:hidden">
-                <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-                    <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                        :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
-                        :aria-current="item.current ? 'page' : undefined">{{ item.name }}
-                    </DisclosureButton>
-                </div>
-                <div class="border-t border-gray-700 pt-4 pb-3">
-                    <div class="flex items-center px-5">
-                        <div class="flex-shrink-0">
-                            <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
-                        </div>
-                        <div class="ml-3">
-
-                            <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
-                            <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
-                        </div>
-
-                        <Button type="button"
-                                class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                            <span class="sr-only">{{ trans("View notifications") }}</span>
-                            <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bell" size="lg" />
-                        </Button>
-                        <Button type="button" class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                            <span class="sr-only">{{ trans("View notifications") }}</span>
-                            <FontAwesomeIcon aria-hidden="true" icon="fa-regular fa-bell" size="lg" />
-
-                        </Button>
-
-                    </div>
-                    <div class="mt-3 space-y-1 px-2">
-                        <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
-                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
-                            {{ item.name }}
-                        </DisclosureButton>
-                    </div>
-                </div>
-            </DisclosurePanel> -->
-        </Disclosure>
+        <AppTopBar :sidebarOpen="sidebarOpen" :logoRoute="`public.dashboard.show`">
+            <img v-if="layout.organisation.logo_id" class="h-7 md:h-5 shadow" :src="`/media/${layout.organisation.logo_id}`" :alt="layout.organisation.code" />
+            <span class="hidden leading-none md:inline font-bold  xl:truncate text-gray-800 dark:text-gray-300">
+                {{ layout.organisation.name}}
+            </span>
+        </AppTopBar>
 
         <!-- Breedcrumbs -->
         <Breadcrumbs class="fixed md:left-10 xl:left-56 top-11 lg:top-10 z-[19] w-full"
