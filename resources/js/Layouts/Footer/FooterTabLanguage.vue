d740d358c1fa808ee04bdaf0dc252f3288db9570
aqordeon <aqordivika@yahoo.co.id>
2023-08-14 10:08:10 +0800

    Public: add Footer Language

--- resources/js/Layouts/Footer/FooterTabLanguage.vue
+++ resources/js/Layouts/Footer/FooterTabLanguage.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+// This file is used on Tenant App, Public
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLanguage } from '@/../private/pro-light-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 library.add(faLanguage)
 import { useLocaleStore } from "@/Stores/locale"
 import FooterTab from '@/Components/Footer/FooterTab.vue'
+import { trans } from 'laravel-vue-i18n'
 
 const locale = useLocaleStore()
 
@@ -29,11 +32,13 @@ defineEmits<{
         </div>
         <FooterTab @pinTab="() => $emit('isTabActive', false)" v-if="isTabActive === 'language'" :tabName="`language`">
             <template #default>
-                <div v-for="(option, index) in locale.languageOptions" :class="[ locale.language.id == index ? 'bg-gray-400 text-gray-100' : 'text-gray-100 hover:bg-gray-500', 'grid py-1.5']"
+                <div v-if="Object.keys(locale.languageOptions).length > 0" v-for="(option, index) in locale.languageOptions"
+                    :class="[ locale.language.id == index ? 'bg-gray-400 text-gray-100' : 'text-gray-100 hover:bg-gray-500', 'grid py-1.5']"
                     @click="locale.language.id = Number(index), locale.language.name = option.label"
                 >
                     {{ option.label }}
                 </div>
+                <div v-else class="grid pt-2.5 pb-1.5">{{ trans('Nothing to show here') }}</div>
             </template>
         </FooterTab>
     </div>
