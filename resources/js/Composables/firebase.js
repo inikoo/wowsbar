34cd22ceb29953776f61c8b786305e5b07e1bb6b
artha <artha@aw-advantage.com>
2023-08-16 11:53:05 +0800

    PKA-1046 Check elasticsearch global search user request for 3 different role user

--- resources/js/Composables/firebase.js
+++ resources/js/Composables/firebase.js
@@ -8,18 +8,29 @@ import {
 
 const credential = await import('/' + import.meta.env.VITE_FIREBASE_CREDENTIALS);
 
-let init = initializeApp(
+/*let init = initializeApp(
     {
         credential   : credential,
         databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
     },
-);
+);*/
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
-/*
- initializeAppCheck(app, {
-    provider: new ReCaptchaEnterpriseProvider(import.meta.env.VITE_RECAPTCHA_APP_KEY),
-});
-*/
+const init = initializeApp(firebaseConfig);
+
+//  initializeAppCheck(init, {
+//     provider: new ReCaptchaEnterpriseProvider(import.meta.env.VITE_RECAPTCHA_APP_KEY),
+// });
+
 let getDb = getDatabase(init);
 
 export const getDbReff = (tenant) => {
