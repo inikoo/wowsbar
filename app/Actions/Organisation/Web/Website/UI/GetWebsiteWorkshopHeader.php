a61638c24e49e9fe864d98c477d3b72c02830bb7
Raul Perusquia <raul@inikoo.com>
2023-08-17 09:48:56 +0800

    org website section 🌎

--- app/Actions/Organisation/Web/Website/UI/GetWebsiteWorkshopHeader.php
+++ app/Actions/Organisation/Web/Website/UI/GetWebsiteWorkshopHeader.php
@@ -7,7 +7,7 @@
 
 namespace App\Actions\Organisation\Web\Website\UI;
 
-use App\Models\Web\Website;
+use App\Models\Organisation\Web\Website;
 use Lorisleiva\Actions\Concerns\AsObject;
 
 class GetWebsiteWorkshopHeader
