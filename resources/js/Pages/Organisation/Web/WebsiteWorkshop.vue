bc7dc759e2239afdbf937af8667f4a95d119acde
aqordeon <aqordivika@yahoo.co.id>
2023-08-21 08:28:55 +0800

    Clean code on Image and WebsiteWorkshop

--- resources/js/Pages/Organisation/Web/WebsiteWorkshop.vue
+++ resources/js/Pages/Organisation/Web/WebsiteWorkshop.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import {Head} from '@inertiajs/vue3';
-import {library} from '@fortawesome/fontawesome-svg-core';
+import { Head } from '@inertiajs/vue3'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-    faArrowAltToTop, faArrowAltToBottom, faBars,faBrowser,faCube,faPalette,faCookieBite
-} from "@/../private/pro-light-svg-icons";
+    faArrowAltToTop, faArrowAltToBottom, faBars, faBrowser, faCube, faPalette, faCookieBite
+} from "@/../private/pro-light-svg-icons"
 
-import PageHeading from '@/Components/Headings/PageHeading.vue';
-import { computed, defineAsyncComponent, ref } from "vue";
-import { useTabChange } from "@/Composables/tab-change";
-import Tabs from "@/Components/Navigation/Tabs.vue";
-import IrisWorkshopHeader from "@/Pages/Iris/IrisWorkshopHeader.vue";
-import IrisWorkshopMenu from "@/Pages/Iris/IrisWorkshopMenu.vue";
-import IrisWorkshopFooter from "@/Pages/Iris/IrisWorkshopFooter.vue";
+import PageHeading from '@/Components/Headings/PageHeading.vue'
+import { computed, defineAsyncComponent, ref } from "vue"
+import { useTabChange } from "@/Composables/tab-change"
+import Tabs from "@/Components/Navigation/Tabs.vue"
+import IrisWorkshopHeader from "@/Pages/Iris/IrisWorkshopHeader.vue"
+import IrisWorkshopMenu from "@/Pages/Iris/IrisWorkshopMenu.vue"
+import IrisWorkshopFooter from "@/Pages/Iris/IrisWorkshopFooter.vue"
 import { capitalize } from "@/Composables/capitalize"
 
 library.add(
@@ -22,38 +22,35 @@ library.add(
     faCube,
     faPalette,
     faCookieBite
-);
+)
 
 
 const props = defineProps<{
     title: string,
     pageHead: object,
     tabs: {
-        current: string;
-        navigation: object;
+        current: string
+        navigation: object
     }
-    color_scheme?: object;
-    header?: object;
-    menu?: object;
-    footer?: object;
-    category?: object;
-    product?: object;
+    color_scheme?: object
+    header?: object
+    menu?: object
+    footer?: object
+    category?: object
+    product?: object
 }>()
 
-let currentTab = ref(props.tabs.current);
-const handleTabUpdate = (tabSlug) => useTabChange(tabSlug, currentTab);
+let currentTab = ref(props.tabs.current)
+const handleTabUpdate = (tabSlug) => useTabChange(tabSlug, currentTab)
 
 const component = computed(() => {
-
     const components = {
-
         header: IrisWorkshopHeader,
         menu: IrisWorkshopMenu,
         footer: IrisWorkshopFooter,
-    };
-    return components[currentTab.value];
-
-});
+    }
+    return components[currentTab.value]
+})
 
 </script>
 
