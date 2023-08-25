c46f6da9e5108224bd0ec0f94b65706e08df813f
aqordeon <aqordivika@yahoo.co.id>
2023-08-25 08:30:30 +0800

    LastEditedBanner: fix route, Image

--- resources/js/Pages/Tenant/Dashboard.vue
+++ resources/js/Pages/Tenant/Dashboard.vue
@@ -8,7 +8,8 @@
 import { Head, Link } from '@inertiajs/vue3'
 import { capitalize } from "@/Composables/capitalize"
 import { trans } from 'laravel-vue-i18n'
-import { useLayoutStore } from '@/Stores/layout';
+import { useLayoutStore } from '@/Stores/layout'
+import Image from '@/Components/Image.vue'
 
 const props = defineProps<{
     title: string,
@@ -44,20 +45,18 @@ console.log(props.banners.data)
         <div v-if="banners" class="max-w-2xl lg:mx-0 lg:max-w-none">
             <div class="flex items-center justify-between">
                 <h2 class="text-base text-gray-700">{{ trans('Recently edited banner') }}</h2>
-                <!-- <a href="#" class="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                <Link :href="route('portfolio.banners.index')" class="text-sm font-semibold text-gray-500 hover:text-gray-700">
                     View all<span class="sr-only">, banner</span>
-                </a> -->
+                </Link>
             </div>
 
             <!-- Looping: Last Edited Banners -->
-
-
             <ul  role="list" class="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
-                <Link :href="`route(lastEditedBanner.route?.name, lastEditedBanner.route?.parameters)`" v-for="lastEditedBanner in banners.data" :key="lastEditedBanner.id" class="overflow-hidden rounded-md ring-1 ring-gray-300 transition-all duration-200 ease-in-out hover:ring-2 hover:ring-gray-400">
-                    <div class="flex items-center gap-x-4 border-b border-gray-700/5 bg-gray-200">
-                        <img :src="lastEditedBanner.components[0]?.image_source" :alt="lastEditedBanner?.name" class="aspect-[4/1]" />
+                <Link :href="route(lastEditedBanner.route?.name, lastEditedBanner.route?.parameters)" v-for="lastEditedBanner in banners.data" :key="lastEditedBanner.id" class="overflow-hidden rounded-md ring-1 ring-gray-300 hover:ring-2 hover:ring-gray-400">
+                    <div class="h-auto aspect-[4/1] flex items-center justify-center gap-x-4 border-b border-gray-700/5 bg-gray-200">
+                        <Image :src="lastEditedBanner.components[0].image.source" :alt="lastEditedBanner?.name" class="" />
                     </div>
-                    <dl class="divide-y divide-transparent px-4 py-3 text-sm">
+                    <dl class="divide-y divide-transparent px-4 pt-1 pb-3 text-sm">
                         <div class="flex justify-between items-center gap-x-4">
                             <!-- <dt class="text-gray-500 text-sm">{{ trans('Name') }}</dt> -->
                             <dd class="flex items-start gap-x-2">
