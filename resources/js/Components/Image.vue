281c7f27411f339f857c5c76a8e6004e0199b9b2
aqordeon <aqordivika@yahoo.co.id>
2023-08-25 13:17:13 +0800

    Image: add fallback if src null

--- resources/js/Components/Image.vue
+++ resources/js/Components/Image.vue
@@ -14,42 +14,45 @@ const props = defineProps<{
     class?: string
 }>()
 
-const { src } = toRefs(props)
+if(props.src){
+    const { src } = toRefs(props)
 
-const imageSrc = ref(cloneDeep(src))
-const avif =  ref(imageSrc.value.avif)
-const webp = ref(imageSrc.value.webp)
-const original = ref(imageSrc.value.original)
+    const imageSrc = ref(cloneDeep(src))
+    const avif = ref(imageSrc.value.avif)
+    const webp = ref(imageSrc.value.webp)
+    const original = ref(imageSrc.value.original)
 
-watch(src, (newValue) => {
-    imageSrc.value = newValue
-    avif.value = newValue.avif
-    webp.value = newValue.webp
-    original.value = newValue.original
-    setImage()
-})
+    watch(src, (newValue) => {
+        imageSrc.value = newValue
+        avif.value = newValue.avif
+        webp.value = newValue.webp
+        original.value = newValue.original
+        setImage()
+    })
 
-const setImage = () => {
-    if (imageSrc.value.avif_2x) {
-        avif.value += ' 1x, ' + imageSrc.value.avif_2x + ' 2x'
+    const setImage = () => {
+        if (imageSrc.value.avif_2x) {
+            avif.value += ' 1x, ' + imageSrc.value.avif_2x + ' 2x'
+        }
+
+        if (imageSrc.value.webp_2x) {
+            webp.value += ' 1x, ' + imageSrc.value.webp_2x + ' 2x'
+        }
+
+        if (imageSrc.value.original_2x) {
+            original.value += ' 1x, ' + imageSrc.value.original_2x + ' 2x'
+        }
     }
 
-    if (imageSrc.value.webp_2x) {
-        webp.value += ' 1x, ' + imageSrc.value.webp_2x + ' 2x'
-    }
-
-    if (imageSrc.value.original_2x) {
-        original.value += ' 1x, ' + imageSrc.value.original_2x + ' 2x'
-    }
+    onBeforeMount(setImage)
 }
-
-onBeforeMount(setImage)
 </script>
 
 <template>
-    <picture>
+    <picture v-if="$props.src">
         <source v-if="src.avif" type="image/avif" :srcset="avif">
         <source v-if="src.webp" type="image/webp" :srcset="webp">
         <img :class="class" :srcset="original" :src="src.original" :alt="alt">
     </picture>
+    <img v-else :class="class" src="@/../art/fallback/fallback.svg" :alt="alt">
 </template>
--- resources/js/Components/LastEditedBanners.vue
+++ resources/js/Components/LastEditedBanners.vue
@@ -17,7 +17,7 @@ const formatDate = (dateIso: Date) => {
         // const hours = date.getHours().toString()
         // const minutes = date.getMinutes().toString()
 
-        return date.toString()
+        return dateIso.toString()
     }
     return ''
 }
@@ -36,8 +36,8 @@ console.log(props)
 
         <!-- Looping: Last Edited Banners -->
         <ul  role="list" class="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
-            <Link :href="route(lastEditedBanner.route?.name, lastEditedBanner.route?.parameters)" v-for="lastEditedBanner in banners.data" :key="lastEditedBanner.id" class="overflow-hidden rounded-md ring-1 ring-gray-300 hover:ring-2 hover:ring-gray-400">
-                <div v-if="lastEditedBanner.image" class="h-auto aspect-[4/1] flex items-center justify-center gap-x-4 border-b border-gray-700/5 bg-gray-200">
+            <Link :href="`${route(lastEditedBanner.route?.name, lastEditedBanner.route?.parameters)}`" v-for="lastEditedBanner in banners.data" :key="lastEditedBanner.id" class="overflow-hidden rounded-md ring-1 ring-gray-300 hover:ring-2 hover:ring-gray-400">
+                <div class="h-auto aspect-[4/1] flex items-center justify-center gap-x-4 border-b border-gray-700/5 bg-gray-200 overflow-hidden">
                     <Image :src="lastEditedBanner.image" :alt="lastEditedBanner?.name" class="" />
                 </div>
                 <dl class="divide-y divide-transparent px-4 pt-1 pb-3 text-sm">
