ed2e3fc1409ac38206769258223d195d89067506
Raul Perusquia <raul@inikoo.com>
2023-08-25 14:33:23 +0800

    fix user logs

--- app/Http/Middleware/LogUserFirebaseMiddleware.php
+++ app/Http/Middleware/LogUserFirebaseMiddleware.php
@@ -20,26 +20,24 @@ class LogUserFirebaseMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $user   = $request->user();
+        $user = $request->user();
 
-        if($user && env('LIVE_USERS_LIST')) {
-
-            if(Auth::getDefaultDriver() == 'web') {
-                $parentTpe  = 'Tenant';
+        if ($user && env('LIVE_USERS_LIST')) {
+            if (Auth::getDefaultDriver() == 'web') {
+                $parentType = 'Tenant';
                 $parentSlug = app('currentTenant')->slug;
             } else {
-                $parentTpe ='Organisation';
-                $parentSlug=null;
+                $parentType = 'Organisation';
+                $parentSlug = null;
             }
 
 
-
-            if($request->route()->getName() == 'logout') {
-                $loggedIn=false;
-                $route   =null;
+            if ($request->route()->getName() == 'logout') {
+                $loggedIn = false;
+                $route    = null;
             } else {
-                $loggedIn=true;
-                $route   = [
+                $loggedIn = true;
+                $route    = [
                     'icon'      => Arr::get($request->route()->action, 'icon'),
                     'label'     => Arr::get($request->route()->action, 'label'),
                     'name'      => request()->route()->getName(),
@@ -47,15 +45,13 @@ class LogUserFirebaseMiddleware
                 ];
             }
 
-            StoreUserLogFirebase::dispatch(
-                user:$user,
-                parentType:$parentTpe,
-                parentSlug:$parentSlug,
-                logedIn:$loggedIn,
-                route:$route
+            StoreUserLogFirebase::run(
+                user: $user,
+                parentType: $parentType,
+                parentSlug: $parentSlug,
+                loggedIn: $loggedIn,
+                route: $route
             );
-
-
         }
 
         return $next($request);
