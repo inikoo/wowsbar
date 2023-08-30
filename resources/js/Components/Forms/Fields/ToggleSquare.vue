82d14e075ca3a744354c1e44dcad0e41871d4d89
aqordeon <aqordivika@yahoo.co.id>
2023-08-30 14:17:49 +0800

    User edit: add field toggleSquare

---
+++ resources/js/Components/Forms/Fields/ToggleSquare.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+    form: any
+    fieldName: string
+    options?: any
+    fieldData: {
+        placeholder: string
+        typeLabel: string[]
+        readonly: boolean
+        copyButton: boolean
+        maxLength?: number
+    }
+}>()
+
+</script>
+
+
+<template>
+    <div class="flex">
+        <div class="grid grid-cols-2 cursor-pointer rounded overflow-hidden text-xs select-none ring-1 ring-gray-300">
+            <div @click="form[fieldName] = false" class="py-1.5 px-2 flex justify-center capitalize transition-all duration-200 ease-in-out" :class="[!form[fieldName] ? 'bg-gray-600 text-gray-100' : 'bg-gray-200/70 text-gray-400 hover:bg-gray-300/70']">{{ fieldData.typeLabel[0] }}</div>
+            <div @click="form[fieldName] = true" class="py-1.5 px-2 flex justify-center capitalize transition-all duration-200 ease-in-out" :class="[form[fieldName] ? 'bg-gray-600 text-gray-100' : 'bg-gray-200/70 text-gray-400 hover:bg-gray-300/70']">{{ fieldData.typeLabel[1] }}</div>
+        </div>
+    </div>
+</template>
