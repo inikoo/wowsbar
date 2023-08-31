aaeec49de967ab499eeb387d828f2f2682b300cf
YudhistaA <aryarajasa0@gmail.com>
2023-08-31 15:04:26 +0800

    filterdata before submit

--- resources/js/Pages/Tenant/Portfolio/BannerWorkshop.vue
+++ resources/js/Pages/Tenant/Portfolio/BannerWorkshop.vue
@@ -12,7 +12,7 @@ import { capitalize } from "@/Composables/capitalize"
 import SlidesWorkshop from "@/Components/Workshop/SlidesWorkshop.vue"
 import Slider from "@/Components/Slider/Slider.vue"
 import SlidesWorkshopAddMode from "@/Components/Workshop/SlidesWorkshopAddMode.vue"
-import { cloneDeep, set as setLodash } from "lodash"
+import { cloneDeep, isEqual, set as setLodash } from "lodash"
 import { set, onValue, get } from "firebase/database"
 import { useLayoutStore } from "@/Stores/layout"
 import { usePage } from "@inertiajs/vue3"
@@ -142,7 +142,6 @@ onValue(getDbRef(dbPath), (snapshot) => {
 })
 
 const updateData = async () => {
-    console.log("update Data")
         try {
             if (data && setData.value == false) {
                 // console.log("Update setLodash")
@@ -163,13 +162,19 @@ const updateData = async () => {
 watch(data, updateData, { deep: true })
 onBeforeMount(fetchInitialData)
 
-const setDataBeforeLeave = () => {
+const deleteUser=()=>{
     const set = { ...data }  // Creating a copy of the data object
     for (const index in set.components) {
         if (set.components[index].user == user.value.username) {
             delete set.components[index].user  // Removing the 'user' property from components
         }
     }
+    if(set.common.user)   delete set.common.user
+    return set
+}
+
+const setDataBeforeLeave = () => {
+    const set = deleteUser()
     Object.assign(data, set)  // Assigning the modified 'set' object back to 'data'
     updateData()  // This line should help you see the modified 'data' object
     autoSave()
@@ -202,7 +207,7 @@ const routeButton = (action) => {
 const sendDataToServer = async () => {
     // When click 'Publish'
     const formValues = {
-        ...data,
+        ...deleteUser(),
         ...(props.banner.state !== 'unpublished' && { comment: comment.value }),
     };
 
@@ -231,14 +236,14 @@ const sendDataToServer = async () => {
 
 
 const autoSave=()=>{
-    const form = useForm(data);
+    const form = useForm(deleteUser());
     form.patch(
         route(props.autoSaveRoute.name,props.autoSaveRoute.parameters), {
             onSuccess: async (res) => {
                 notify({
                     title: "success Update",
                     type: "success",
-                    text: "Banner already update and publish",
+                    text: "test aoutosave",
                 });
             },
             onError: errors => {
