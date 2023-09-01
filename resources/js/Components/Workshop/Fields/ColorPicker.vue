d72e15263de2cfcbf9dee46c1901f0cec4c2d300
aqordeon <aqordivika@yahoo.co.id>
2023-09-01 10:03:09 +0800

    PKA-1114 SlideWorkshop: UI ColorPicker

--- resources/js/Components/Workshop/Fields/ColorPicker.vue
+++ resources/js/Components/Workshop/Fields/ColorPicker.vue
@@ -4,7 +4,11 @@ import { ColorPicker } from 'vue-color-kit'
 import 'vue-color-kit/dist/vue-color-kit.css'
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
 import { set } from 'lodash'
-import { ref, watch, defineEmits } from 'vue'
+import { ref, watch } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPaintBrushAlt } from '@/../private/pro-regular-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faPaintBrushAlt)
 
 const props = defineProps<{
     fieldName: string | []
@@ -63,33 +67,22 @@ const updateFormValue = (newValue) => {
 </script>
 
 <template>
-    <div class="flex gap-5">
-        <div>
-            <div class="bg-gray-950 border-black rounded-full w-10 h-10 justify-center" :style="`border: 1px solid`"
-                @click="() => changeColor({ rgba: { r: 0, g: 0, b: 0, a: 1 } })" />
-            <div class="flex items-center justify-center text-sm font-semibold">Black</div>
-        </div>
-        <div>
-            <div class="bg-white border-black rounded-full w-10 h-10 justify-center" :style="`border: 1px solid`"
-                @click="() => changeColor({ rgba: { r: 255, g: 255, b: 255, a: 255 } })" />
-            <div class="flex items-center justify-center text-sm font-semibold">White</div>
-        </div>
-
+    <div class="flex gap-3">
         <Popover v-slot="{ open }">
             <div class="relative">
                 <PopoverButton>
-                    <div class="bg-gray-950 border-black rounded-full w-10 h-10 justify-center"
-                        :style="`background-color: ${color}; border: 1px solid`" />
-                    <div class="flex items-center justify-center text-sm font-semibold">Custom</div>
+                    <div class="border border-slate-300 rounded-full w-10 h-10 flex justify-center items-center"
+                        :style="`background-color: ${color}`" >
+                        <FontAwesomeIcon icon='far fa-paint-brush-alt' class='text-gray-300 text-lg' aria-hidden='true' />
+                    </div>
                 </PopoverButton>
 
                 <Transition enter-active-class="transition duration-200 ease-out" enter-from-class="translate-y-1 opacity-0"
                     enter-to-class="translate-y-0 opacity-100" leave-active-class="transition duration-150 ease-in"
                     leave-from-class="translate-y-0 opacity-100" leave-to-class="translate-y-1 opacity-0">
-                    <PopoverPanel v-show="open"
-                        class="absolute bottom-full left-1/2 z-10 mb-3 -translate-x-1/2 transform px-4 sm:px-0">
+                    <PopoverPanel v-show="open" class="absolute bottom-full left-1/2 z-10 mb-3 -translate-x-1/2 transform px-4 sm:px-0">
                         <div :style="{ background: color }">
-                            <ColorPicker theme="light" v-model="color" :sucker-hide="false" @changeColor="changeColor"
+                            <ColorPicker theme="light" v-model="color" @changeColor="changeColor"
                                 style="width: 225px;" />
                         </div>
                     </PopoverPanel>
@@ -97,6 +90,28 @@ const updateFormValue = (newValue) => {
             </div>
         </Popover>
 
+        <!--  -->
+        <div class="flex gap-x-1 items-center">
+            <div class="bg-gray-700 border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 55, g: 65, b: 81, a: 1 } })" />
+            <div class="bg-white border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 255, g: 255, b: 255, a: 255 } })" />
+            <div class="bg-yellow-300 border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 253, g: 224, b: 71, a: 255 } })" />
+            <div class="bg-emerald-500 border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 16, g: 185, b: 129, a: 255 } })" />
+            <div class="bg-sky-500 border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 14, g: 165, b: 233, a: 255 } })" />
+            <div class="bg-fuchsia-500 border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 99, g: 102, b: 241, a: 255 } })" />
+            <div class="bg-rose-500 border border-slate-300 rounded w-6 h-6 shadow cursor-pointer hover:scale-110 transition-transform duration-100 ease-in-out" @click="() => changeColor({ rgba: { r: 244, g: 63, b: 94, a: 255 } })" />
+        </div>
 
-</div></template>
-  
+        
+
+
+    </div>
+</template>
+
+<style lang="scss">
+.colors {
+    @apply hidden
+}
+
+
+</style>
+
