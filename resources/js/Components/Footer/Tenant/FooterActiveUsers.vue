bf8c6688ad0ed70baf742efb35f64207657582b5
aqordeon <aqordivika@yahoo.co.id>
2023-09-08 11:16:21 +0800

    FooterActiveUsers: fix active users, 'away' status

--- resources/js/Components/Footer/Tenant/FooterActiveUsers.vue
+++ resources/js/Components/Footer/Tenant/FooterActiveUsers.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 import { trans } from 'laravel-vue-i18n'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useLayoutStore } from "@/Stores/layout"
 import FooterTab from '@/Components/Footer/FooterTab.vue'
 import { faBriefcase} from "@/../private/pro-light-svg-icons";
@@ -14,9 +14,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { getDataFirebase } from '@/Composables/firebase'
 import { watchEffect } from 'vue'
-// import moment from "moment";
 
-library.add(faBriefcase);
+// library.add(faBriefcase);
 
 const props = defineProps<{
     isTabActive: string | boolean
@@ -34,21 +33,28 @@ const getDataTenant = ref(getDataFirebase(dbPath))
 const dataTenant = ref()
 const dataTenantLength = ref()
 
+const compUserOnline = computed(() => {
+    return dataTenant.value.filter((item: any) => item.loggedIn === true)
+})
+
 watchEffect(() => {
     dataTenant.value = getDataTenant.value
-    dataTenantLength.value = dataTenant.value ? Object.keys(dataTenant.value).length : 0
+    console.log(dataTenant.value)
+    dataTenantLength.value = compUserOnline.value ? Object.keys(compUserOnline.value).length : 0
     layout.rightSidebar.activeUsers.users = dataTenant.value
     layout.rightSidebar.activeUsers.count = dataTenantLength.value
 })
 
-function getAwayStatus(lastActive)
-{
-    // lastActive = moment(lastActive);
-    // let now = moment();
-
-    // return Boolean(now.diff(lastActive));
-    return 'moment jsss'
+const getStatusOnline = (dataUser: any) => {
+    // if stay on the same page for over than 10 minute return true
+    const lastActive = new Date(dataUser.last_active)
+    const currentTime = new Date()
+    const timeDifference = Math.floor((currentTime - lastActive) / (1000 * 60))
+    
+    return timeDifference < 11
 }
+
+
 
 </script>
 
@@ -64,17 +70,10 @@ function getAwayStatus(lastActive)
         </div>
         <FooterTab @pinTab="() => $emit('isTabActive', false)" v-if="isTabActive == 'activeUsers'" :tabName="`activeUsers`">
             <template #default>
-                <div v-if="dataTenantLength" v-for="(dataUser, index) in dataTenant" class="flex justify-start py-1 px-2 gap-x-1.5 hover:bg-gray-700 cursor-default">
+                <div v-if="dataTenantLength" v-for="(dataUser, index) in compUserOnline" class="flex justify-start py-1 px-2 gap-x-1.5 hover:bg-gray-700 cursor-default">
                     <!-- <img :src="`/media/${user.user.avatar_thumbnail}`" :alt="user.user.contact_name" srcset="" class="h-4 rounded-full shadow"> -->
-                    <span class="font-semibold text-gray-100">{{ dataUser.id }}</span> -
-                    <!-- <FontAwesomeIcon
-                        v-if="dataUser.route.icon"
-                        class="flex-shrink-0 h-3 w-3 mr-1 opacity-80"
-                        :icon="'fal fa-'+dataUser.route.icon"
-                        aria-hidden="true" /> -->
-                    <span v-if="dataUser.loggedIn" class="text-gray-300">{{ dataUser.route?.name ? trans(dataUser.route.name) : '' }}</span>
-                    <span v-else-if="getAwayStatus(dataUser.last_active)" class="text-gray-300">{{ getAwayStatus(dataUser.last_active) ? 'Away' : '' }}</span>
-                    <!-- <span v-if="dataUser.route.subject" class="capitalize text-gray-300">{{ dataUser.route.subject }}</span> -->
+                    <span class="font-semibold" :class="[getStatusOnline(dataUser) ? 'text-gray-100' : 'text-gray-400']">{{ dataUser.id }}</span> -
+                    <span v-if="dataUser.loggedIn" class="" :class="[getStatusOnline(dataUser) ? 'text-gray-300' : 'text-gray-400']">{{ getStatusOnline(dataUser) ? dataUser.route.name : 'Away' }}</span>
                 </div>
             </template>
         </FooterTab>
