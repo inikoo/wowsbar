0ca837c616c8b8ddff62cd2fbed02319960b4467
artha <artha@aw-advantage.com>
2023-09-08 08:31:43 +0800

    PKA-1131 Upload website backend

--- app/Events/UploadWebsiteProgressEvent.php
+++ app/Events/UploadWebsiteProgressEvent.php
@@ -5,13 +5,12 @@ namespace App\Events;
 use App\Models\Tenancy\Tenant;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UploadWebsiteProgressEvent implements ShouldBroadcast
+class UploadWebsiteProgressEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
