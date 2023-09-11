121a4327c1312ba149cdf05cdd0d1bc0e507cf74
artha <artha@aw-advantage.com>
2023-09-11 11:08:36 +0800

    PKA-1137 create model customers

---
+++ app/Models/CRM/Customer.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 20 Jun 2023 20:21:10 Malaysia Time, Pantai Lembeng, Bali, Id
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Models\CRM;
+
+use App\Actions\Market\Shop\Hydrators\ShopHydrateCustomerInvoices;use App\Enums\CRM\Customer\CustomerStateEnum;use App\Enums\CRM\Customer\CustomerStatusEnum;use App\Enums\CRM\Customer\CustomerTradeStateEnum;use App\Models\Accounting\Invoice;use App\Models\Accounting\Payment;use App\Models\Auth\WebUser;use App\Models\Dropshipping\CustomerClient;use App\Models\Fulfilment\FulfilmentOrder;use App\Models\Fulfilment\StoredItem;use App\Models\Helpers\Address;use App\Models\Helpers\Issue;use App\Models\Helpers\TaxNumber;use App\Models\Inventory\Stock;use App\Models\Market\Product;use App\Models\Market\Shop;use App\Models\Media\GroupMedia;use App\Models\OMS\Order;use App\Models\Search\UniversalSearch;use App\Models\Traits\HasPhoto;use App\Models\Traits\HasTenantAddress;use App\Models\Traits\HasUniversalSearch;use Eloquent;use Illuminate\Database\Eloquent\Builder;use Illuminate\Database\Eloquent\Collection;use Illuminate\Database\Eloquent\Factories\HasFactory;use Illuminate\Database\Eloquent\Model;use Illuminate\Database\Eloquent\Relations\BelongsTo;use Illuminate\Database\Eloquent\Relations\HasMany;use Illuminate\Database\Eloquent\Relations\HasOne;use Illuminate\Database\Eloquent\Relations\MorphMany;use Illuminate\Database\Eloquent\Relations\MorphOne;use Illuminate\Database\Eloquent\Relations\MorphToMany;use Illuminate\Database\Eloquent\SoftDeletes;use Illuminate\Support\Carbon;use Spatie\MediaLibrary\HasMedia;use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;use Spatie\Sluggable\HasSlug;use Spatie\Sluggable\SlugOptions;
+
+/**
+ * App\Models\CRM\Customer
+ *
+ * @property int $id
+ * @property int|null $shop_id
+ * @property int|null $image_id
+ * @property string $slug
+ * @property string|null $reference customer public id
+ * @property string|null $name
+ * @property string|null $contact_name
+ * @property string|null $company_name
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $identity_document_type
+ * @property string|null $identity_document_number
+ * @property string|null $contact_website
+ * @property array $location
+ * @property CustomerStatusEnum $status
+ * @property CustomerStateEnum $state
+ * @property CustomerTradeStateEnum $trade_state number of invoices
+ * @property bool $is_fulfilment
+ * @property bool $is_dropshipping
+ * @property array $data
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property int|null $source_id
+ * @property-read Collection<int, Address> $addresses
+ * @property-read \App\Models\CRM\CustomerStats|null $stats
+ * @property-read UniversalSearch|null $universalSearch
+ * @method static Builder|Customer newModelQuery()
+ * @method static Builder|Customer newQuery()
+ * @method static Builder|Customer onlyTrashed()
+ * @method static Builder|Customer query()
+ * @method static Builder|Customer withTrashed()
+ * @method static Builder|Customer withoutTrashed()
+ * @mixin Eloquent
+ */
+class Customer extends Model implements HasMedia
+{
+    use UsesTenantConnection;
+    use SoftDeletes;
+    use HasSlug;
+    use HasUniversalSearch;
+    use HasPhoto;
+    use HasFactory;
+
+    protected $casts = [
+        'data'            => 'array',
+        'location'        => 'array',
+        'state'           => CustomerStateEnum::class,
+        'status'          => CustomerStatusEnum::class,
+        'trade_state'     => CustomerTradeStateEnum::class
+
+    ];
+
+    protected $attributes = [
+        'data'            => '{}',
+        'location'        => '{}',
+    ];
+
+    protected $guarded = [];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('reference')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(
+            function (Customer $customer) {
+                $customer->name = $customer->company_name == '' ? $customer->contact_name : $customer->company_name;
+            }
+        );
+
+        static::updated(function (Customer $customer) {
+            if ($customer->wasChanged(['contact_name', 'company_name'])) {
+                $customer->name = $customer->company_name == '' ? $customer->contact_name : $customer->company_name;
+            }
+        });
+    }
+
+    public function stats(): HasOne
+    {
+        return $this->hasOne(CustomerStats::class);
+    }
+}
