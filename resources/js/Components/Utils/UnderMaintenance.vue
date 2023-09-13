850356ef07c2a2ceb2aa7a864694e39e81b6296d
aqordeon <aqordivika@yahoo.co.id>
2023-09-13 15:01:50 +0800

    [Org] Website Showcase: update data on Button Launch

--- resources/js/Components/Utils/UnderMaintenance.vue
+++ resources/js/Components/Utils/UnderMaintenance.vue
@@ -22,7 +22,7 @@ const props = defineProps<{
 const handleLaunch = () => {
     router.patch(
         route('org.models.website.state.update'),
-        { status: 'live'},
+        { state: 'live'},
     )
 }
 </script>
