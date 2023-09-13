95479e6ad0b3579fccfd443e3a936e8aa63892a7
Raul Perusquia <raul@inikoo.com>
2023-09-13 09:21:36 +0800

    website api resource 🚆

---
+++ app/Http/Resources/Web/WebsiteResource.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 13 Sep 2023 09:52:21 Malaysia Time, Pantai Lembeng, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Http\Resources\Web;
+
+use App\Http\Resources\HasSelfCall;
+use App\Models\Organisation\Web\Website;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+class WebsiteResource extends JsonResource
+{
+
+    use HasSelfCall;
+
+    public function toArray($request): array
+    {
+        /** @var Website $website */
+        $website = $this;
+
+        return [
+            'slug'       => $website->slug,
+            'state'      => $website->state,
+            'status'     => $website->status,
+            'created_at' => $website->created_at,
+            'updated_at' => $website->updated_at,
+        ];
+    }
+}
