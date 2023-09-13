3121deee10173c7d3fa68b3e2bae3a92c878997e
artha <artha@aw-advantage.com>
2023-09-13 10:57:42 +0800

    copy hydrators shops

---
+++ app/Actions/Organisation/Market/Shop/Hydrators/ShopHydrateUniversalSearch.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Sat, 11 Mar 2023 16:06:25 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Market\Shop\Hydrators;
+
+use App\Actions\Traits\WithTenantJob;
+use App\Models\Market\Shop;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ShopHydrateUniversalSearch
+{
+    use AsAction;
+    use WithTenantJob;
+
+    public function handle(Shop $shop): void
+    {
+        $shop->universalSearch()->updateOrCreate(
+            [],
+            [
+                'section'     => 'shops',
+                'title'       => trim($shop->code.' '.$shop->name),
+                'description' => ''
+            ]
+        );
+    }
+
+}
