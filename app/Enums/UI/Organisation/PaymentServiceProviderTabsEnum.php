6ca2479ea1d95563080a51ebfa7518c9cbe87a4f
artha <artha@aw-advantage.com>
2023-09-15 17:02:34 +0800

    Copy tabs enum

---
+++ app/Enums/UI/Organisation/PaymentServiceProviderTabsEnum.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Sun, 19 Mar 2023 01:54:57 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Enums\UI\Organisation;
+
+use App\Enums\EnumHelperTrait;
+use App\Enums\HasTabs;
+
+enum PaymentServiceProviderTabsEnum: string
+{
+    use EnumHelperTrait;
+    use HasTabs;
+
+    case SHOWCASE         = 'showcase';
+    case STATS            = 'stats';
+    case PAYMENT_ACCOUNTS = 'payment_accounts';
+    case PAYMENTS         = 'payments';
+    case HISTORY          = 'history';
+    case DATA             = 'data';
+
+
+    public function blueprint(): array
+    {
+        return match ($this) {
+            PaymentServiceProviderTabsEnum::STATS             => [
+                'title' => __('stats'),
+                'icon'  => 'fal fa-chart-line',
+            ],
+            PaymentServiceProviderTabsEnum::PAYMENT_ACCOUNTS => [
+                'title' => __('payment accounts'),
+                'icon'  => 'fal fa-money-check-alt',
+            ],
+            PaymentServiceProviderTabsEnum::PAYMENTS     => [
+                'title' => __('payments'),
+                'icon'  => 'fal fa-coins',
+            ],
+            PaymentServiceProviderTabsEnum::DATA     => [
+                'title' => __('data'),
+                'icon'  => 'fal fa-database',
+                'type'  => 'icon',
+                'align' => 'right',
+            ],
+            PaymentServiceProviderTabsEnum::HISTORY     => [
+                'title'  => __('history'),
+                'icon'   => 'fal fa-clock',
+                'type'   => 'icon',
+                'align'  => 'right',
+            ],
+            PaymentServiceProviderTabsEnum::SHOWCASE => [
+                'title' => __('payment service provider'),
+                'icon'  => 'fas fa-info-circle',
+            ],
+        };
+    }
+}
---
+++ app/Enums/UI/Organisation/PaymentTabsEnum.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Sun, 19 Mar 2023 01:54:57 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Enums\UI\Organisation;
+
+use App\Enums\EnumHelperTrait;
+use App\Enums\HasTabs;
+
+enum PaymentTabsEnum: string
+{
+    use EnumHelperTrait;
+    use HasTabs;
+
+    case DATA          = 'data';
+    case HISTORY_NOTES = 'history_notes';
+
+    public function blueprint(): array
+    {
+        return match ($this) {
+            PaymentTabsEnum::DATA => [
+                'title' => __('data'),
+                'icon'  => 'fal fa-database',
+                'type'  => 'icon',
+                'align' => 'right',
+            ],
+
+            PaymentTabsEnum::HISTORY_NOTES => [
+                'title' => __('history, notes'),
+                'icon'  => 'fal fa-clock',
+                'type'  => 'icon',
+                'align' => 'right',
+            ],
+        };
+    }
+}
