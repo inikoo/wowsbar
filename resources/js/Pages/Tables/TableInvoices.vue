2e454e12a7fce9e90784093c5a1ee16249f57091
artha <artha@aw-advantage.com>
2023-09-15 17:02:50 +0800

    Add tables

---
+++ resources/js/Pages/Tables/TableInvoices.vue
@@ -0,0 +1,43 @@
+<!--
+  - Author: Raul Perusquia <raul@inikoo.com>
+  - Created: Mon, 20 Mar 2023 23:18:59 Malaysia Time, Kuala Lumpur, Malaysia
+  - Copyright (c) 2023, Raul A Perusquia Flores
+  -->
+
+<script setup lang="ts">
+import {Link} from '@inertiajs/vue3';
+import Table from '@/Components/Table/Table.vue';
+
+const props = defineProps<{
+    data: object,
+    tab?: string
+}>()
+
+
+function invoiceRoute(invoice) {
+    switch (route().current()) {
+        case 'shops.show.invoices.index':
+            return route(
+                'shops.show.invoices.show',
+                [invoice.slug, invoice.slug]);
+        default:
+            return route(
+                'accounting.invoices.show',
+                [invoice.slug]);
+    }
+}
+
+</script>
+
+<template>
+    <Table :resource="data" :name="tab" class="mt-5">
+        <template #cell(number)="{ item: invoice }">
+            <Link :href="invoiceRoute(invoice)">
+                {{ invoice["number"]}}
+            </Link>
+        </template>
+
+    </Table>
+</template>
+
+
