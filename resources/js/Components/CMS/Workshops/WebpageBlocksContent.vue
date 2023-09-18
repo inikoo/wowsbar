627b53ff7a0cca510c0fcedfb5077324610e1e48
YudhistaA <aryarajasa0@gmail.com>
2023-09-18 13:39:02 +0800

    stats block components

--- resources/js/Components/CMS/Workshops/WebpageBlocksContent.vue
+++ resources/js/Components/CMS/Workshops/WebpageBlocksContent.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import BannerWorkshopComponent from '@/Components/Workshop/BannerWorkshopComponent.vue'
 import TestimonialsWorkshopComponent from '@/Components/CMS/BlocksContent/Testimonials/TestimonialsWorkshopComponent.vue'
-// import BlogSectionOne from '@/Components/CMS/BlocksContent/BlogSections/BlogSectionOne.vue'
-// import BlogSectionTwo from '@/Components/CMS/BlocksContent/BlogSections/BlogSectionTwo.vue'
-// import BlogSectionThree from '@/Components/CMS/BlocksContent/BlogSections/BlogSectionThree.vue'
+import Stats from '@/Components/CMS/BlocksContent/Stats/index.vue';
 import BlogSection from '@/Components/CMS/BlocksContent/BlogSections/index.vue'
 
 const props = defineProps<{
@@ -77,5 +75,5 @@ const dummyData = {
         <BannerWorkshopComponent v-if="selectedComponent.component === 'banner'" :data="dummyData.data" :imagesUploadRoute="dummyData.route" :user="dummyData.user" />
         <TestimonialsWorkshopComponent v-if="selectedComponent.component === 'text'" />
     </div>
-    <!-- <BlogSection :theme="2"></BlogSection> -->
+    <Stats :theme="3"></Stats>
 </template>
