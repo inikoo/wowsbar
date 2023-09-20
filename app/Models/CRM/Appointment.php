80a003582983a1337adae933abcf63f1c5c62c9a
artha <artha@aw-advantage.com>
2023-09-20 17:01:56 +0800

    Create appointment model and migrations

---
+++ app/Models/CRM/Appointment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\CRM;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/tenant/2023_09_20_083726_create_appointments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Enums\CRM\Appointment\AppointmentEventEnum;
+use App\Enums\CRM\Appointment\AppointmentStateEnum;
+use App\Enums\CRM\Appointment\AppointmentTypeEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->unsignedInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unsignedSmallInteger('organisation_user_id');
+            $table->foreign('organisation_user_id')->references('id')->on('organisation_users');
+
+            $table->morphs('parent');
+            $table->dateTimeTz('datetime');
+            $table->string('description')->nullable();
+
+            $table->string('state')->default(AppointmentStateEnum::BOOKED->value);
+            $table->string('type');
+
+            $table->string('event');
+            $table->string('event_address');
+
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
