16aac64ea614ad6b3cd2b47a5ef17028c5f4efd1
artha <artha@aw-advantage.com>
2023-09-27 14:40:12 +0800

    Create social account model

---
+++ database/migrations/landlord/2023_09_27_060654_create_portfolio_social_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portfolio_social_accounts', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('username');
+            $table->string('url');
+            $table->string('provider');
+            $table->unsignedBigInteger('number_followers');
+            $table->unsignedBigInteger('number_posts');
+
+            $table->unsignedInteger('customer_id')->index();
+            $table->foreign('customer_id')->references('id')->on('customers')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unsignedSmallInteger('shop_id')->index();
+            $table->foreign('shop_id')->references('id')->on('shops');
+
+            $table->jsonb('data');
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portfolio_social_accounts');
+    }
+};
