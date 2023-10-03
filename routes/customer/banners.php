c6700e760df4523aa7c252eaa97269d2a1fbf993
Raul Perusquia <raul@inikoo.com>
2023-10-03 14:34:41 +0800

    customer permissions 🔐 preparations

--- routes/customer/banners.php
+++ routes/customer/banners.php
@@ -18,57 +18,14 @@ use App\Actions\Portfolio\Gallery\UI\ShowGallery;
 use App\Actions\Portfolio\Gallery\UI\UploadedImages\EditUploadedImage;
 use App\Actions\Portfolio\Gallery\UI\UploadedImages\IndexUploadedImages;
 use App\Actions\Portfolio\Gallery\UI\UploadedImages\ShowUploadedImage;
-use App\Actions\Portfolio\PortfolioWebsite\UI\CreatePortfolioWebsite;
-use App\Actions\Portfolio\PortfolioWebsite\UI\EditPortfolioWebsite;
-use App\Actions\Portfolio\PortfolioWebsite\UI\IndexPortfolioWebsites;
-use App\Actions\Portfolio\PortfolioWebsite\UI\RemovePortfolioWebsite;
-use App\Actions\Portfolio\PortfolioWebsite\UI\ShowPortfolioWebsite;
+use App\Actions\Portfolio\PortfolioWebsite\UI\IndexBannerPortfolioWebsites;
 use App\Actions\Portfolio\Snapshot\UI\IndexSnapshots;
 use App\Actions\Portfolio\Snapshot\UI\ShowSnapshot;
-use App\Actions\Portfolio\Uploads\DownloadPortfolioWebsiteUploadsTemplate;
-use App\Actions\Portfolio\Uploads\IndexPortfolioWebsiteUploads;
 use App\Actions\UI\Customer\Banners\ShowBannersDashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', [
-    'uses'  => ShowBannersDashboard::class,
-    'icon'  => 'rectangle-wide',
-    'label' => 'banners dashboard'
-])->name('dashboard');
-
-
-Route::get('/websites', [
-    'uses'  => IndexPortfolioWebsites::class,
-    'icon'  => 'globe',
-    'label' => 'websites'
-])->name('websites.index');
-/*
-Route::get('/websites/create', CreatePortfolioWebsite::class)->name('websites.create');
-Route::get('/websites/{portfolioWebsite}', [
-        'uses'  => ShowPortfolioWebsite::class,
-        'icon'  => 'globe',
-        'label' => 'websites'
-    ])->name('websites.show');
-Route::get('/websites/{portfolioWebsite}/edit', EditPortfolioWebsite::class)->name('websites.edit');
-Route::get('/websites/{portfolioWebsite}/delete', RemovePortfolioWebsite::class)->withTrashed()->name('websites.remove');
-
-Route::get('/websites/{portfolioWebsite}/banners/create', [CreateBanner::class, 'inPortfolioWebsite'])->name('websites.show.banners.create');
-Route::get('/websites/{portfolioWebsite}/banners', [IndexBanners::class, 'inPortfolioWebsite'])->name('websites.show.banners.index');
-
-
-
-
-
-
-Route::get('websites/{portfolioWebsite}/banners/{banner}/snapshots', [IndexSnapshots::class, 'inWebsite'])->name('websites.snapshots.index');
-Route::get('websites/{portfolioWebsite}/banners/{banner}/snapshots/{snapshot}', [ShowSnapshot::class, 'inWebsite'])->name('websites.snapshots.show');
-
-
-Route::get('/portfolio-websites/uploads/history', IndexPortfolioWebsiteUploads::class)->name('website.uploads.history');
-Route::get('/portfolio-websites/uploads/template/download', DownloadPortfolioWebsiteUploadsTemplate::class)->name('website.uploads.template.download');
-*/
-
-
+Route::get('/dashboard', ['icon'  => 'rectangle-wide', 'label' => 'banners dashboard'])->uses(ShowBannersDashboard::class)->name('dashboard');
+Route::get('/websites', ['icon' => 'globe', 'label' => 'websites'])->uses(IndexBannerPortfolioWebsites::class)->name('websites.index');
 
 
 Route::prefix('gallery')->group(function () {
@@ -87,4 +44,6 @@ Route::prefix('{banner}')->group(function () {
     Route::get('edit', ['icon' => 'globe', 'label' => 'banner'])->uses(EditBanner::class)->name('edit');
     Route::get('workshop', ['icon' => 'globe', 'label' => 'banner'])->uses(ShowBannerWorkshop::class)->name('workshop');
     Route::get('delete', ['icon' => 'globe', 'label' => 'banner'])->uses(RemoveBanner::class)->name('remove');
+    Route::get('snapshots', [IndexSnapshots::class, 'inBanner'])->name('show.snapshots.index');
+    Route::get('snapshots/{snapshot}', [ShowSnapshot::class, 'inBanner'])->name('show.snapshots.show');
 });
