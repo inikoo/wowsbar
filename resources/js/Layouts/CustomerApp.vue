1a3d411a8e8249bb1ce8fb7794247d3ae517b931
aqordeon <aqordivika@yahoo.co.id>
2023-10-05 08:31:25 +0800

    UI: Fix TopBar hover on lg view

--- resources/js/Layouts/CustomerApp.vue
+++ resources/js/Layouts/CustomerApp.vue
@@ -6,99 +6,106 @@
   -  Version 4.0
   -->
 <script setup lang="ts">
-  import { ref } from "vue";
-  import { usePage } from "@inertiajs/vue3"
-  import Footer from "@/Layouts/Customer/Footer.vue"
-  import AppLeftSideBar from "@/Layouts/Customer/AppLeftSideBar.vue"
-  import AppRightSideBar from "@/Layouts/Customer/AppRightSideBar.vue"
-  import TopBar from "@/Layouts/Customer/TopBar.vue"
-  import Breadcrumbs from "@/Components/Navigation/Breadcrumbs.vue"
-  import { library } from "@fortawesome/fontawesome-svg-core"
-  import { initialiseCustomerApp } from "@/Composables/initialiseCustomerApp"
-  import { useLayoutStore } from "@/Stores/layout"
-  import { useAuthFirebase } from "@/Composables/firebaseAuth"
+import { ref } from "vue";
+import { usePage } from "@inertiajs/vue3"
+import Footer from "@/Layouts/Customer/Footer.vue"
+import AppLeftSideBar from "@/Layouts/Customer/AppLeftSideBar.vue"
+import AppRightSideBar from "@/Layouts/Customer/AppRightSideBar.vue"
+import TopBar from "@/Layouts/Customer/TopBar.vue"
+import Breadcrumbs from "@/Components/Navigation/Breadcrumbs.vue"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { initialiseCustomerApp } from "@/Composables/initialiseCustomerApp"
+import { useLayoutStore } from "@/Stores/layout"
+import { useAuthFirebase } from "@/Composables/firebaseAuth"
 
 
-  import {
-      faHome,
-      faBars,
-      faUsersCog,
-      faTachometerAltFast,
-      faUser,
-      faLanguage
-  } from "@/../private/pro-light-svg-icons"
-  import { faSearch, faBell} from "@/../private/pro-regular-svg-icons"
+import {
+    faHome,
+    faBars,
+    faUsersCog,
+    faTachometerAltFast,
+    faUser,
+    faLanguage
+} from "@/../private/pro-light-svg-icons"
+import { faSearch, faBell } from "@/../private/pro-regular-svg-icons"
 
 
-  library.add(
-      faHome,
-      faBars,
-      faUsersCog,
-      faTachometerAltFast,
-      faUser,
-      faLanguage,
-      faSearch,
-      faBell
-  )
+library.add(
+    faHome,
+    faBars,
+    faUsersCog,
+    faTachometerAltFast,
+    faUser,
+    faLanguage,
+    faSearch,
+    faBell
+)
 
-  const sidebarOpen = ref(false)
-  const layout = initialiseCustomerApp()
-
-
-  if (usePage().props.firebaseAuthToken) {
-      useAuthFirebase(usePage().props.firebaseAuthToken)
-  }
-
-  const layoutState = useLayoutStore()
+const sidebarOpen = ref(false)
+const layout = initialiseCustomerApp()
 
 
-  </script>
+if (usePage().props.firebaseAuthToken) {
+    useAuthFirebase(usePage().props.firebaseAuthToken)
+}
 
-  <template>
-      <div class="fixed top-0 left-0 w-screen h-screen dark:bg-gray-700 bg-gray-50" />
-      <div class="relative min-h-full transition-all duration-200 ease-in-out"
-          :class="[Object.values(layout.rightSidebar).some(value => value.show) ? 'mr-44' : 'mr-0']"
-      >
-          <TopBar @sidebarOpen="(value: boolean) => sidebarOpen = value" :sidebarOpen="sidebarOpen" :logoRoute="`customer.dashboard.show`" urlPrefix="customer.">
+const layoutState = useLayoutStore()
 
 
-              <span class="hidden leading-none md:inline font-bold xl:truncate text-gray-800 dark:text-gray-300">
-                  {{ layout.app.name}}
-              </span>
-          </TopBar>
+</script>
 
-          <!-- Breadcrumbs -->
-          <Breadcrumbs class="fixed top-11 z-[19] w-full md:top-11 lg:top-10 "
-              :class="[layoutState.leftSidebar.show ? 'left-0 md:left-56' : 'left-0 md:left-10']"
-              :breadcrumbs="usePage().props.breadcrumbs??[]"
-              :navigation="usePage().props.navigation??[]"
-          />
+<template>
+    <div class="fixed top-0 left-0 w-screen h-screen dark:bg-gray-700 bg-gray-50" />
+    <div class="relative min-h-full transition-all duration-200 ease-in-out"
+        :class="[Object.values(layout.rightSidebar).some(value => value.show) ? 'mr-44' : 'mr-0']">
+        <TopBar @sidebarOpen="(value: boolean) => sidebarOpen = value" :sidebarOpen="sidebarOpen"
+            :logoRoute="`customer.dashboard.show`" urlPrefix="customer.">
+            <span class="hidden leading-none md:inline font-bold xl:truncate text-gray-800 dark:text-gray-300">
+                {{ layout.app.name }}
+            </span>
+        </TopBar>
 
-          <!-- Sidebar: Left -->
-          <div>
-              <div class="bg-gray-200/80 fixed top-0 w-screen h-screen z-10 md:hidden transition-all duration-500 ease-in-out" v-if="sidebarOpen" @click="sidebarOpen = !sidebarOpen" /> <!-- Mobile Helper: background to close hamburger -->
-              <AppLeftSideBar class="-left-2/3 transition-all duration-300 ease-in-out z-20 block md:left-0" :class="{'left-0': sidebarOpen }" @click="sidebarOpen = !sidebarOpen" />
-          </div>
+        <!-- Breadcrumbs -->
+        <Breadcrumbs class="fixed top-11 z-[19] w-full md:top-11 lg:top-10 "
+            :class="[layoutState.leftSidebar.show ? 'left-0 md:left-56' : 'left-0 md:left-10']"
+            :breadcrumbs="usePage().props.breadcrumbs ?? []" :navigation="usePage().props.navigation ?? []" />
 
-          <!-- Main Content -->
-          <main class="relative flex flex-col pt-20 pb-5 md:pt-[68px] lg:pt-16 bg-gray-50 text-gray-700 dark:bg-gray-700 dark:text-gray-400 transition-all duration-200 ease-in-out"
-              :class="[layoutState.leftSidebar.show ? 'ml-0 md:ml-56' : 'ml-0 md:ml-10']"
-          >
-              <slot />
-          </main>
+        <!-- Sidebar: Left -->
+        <div>
+            <Transition>
+                <div class="bg-gray-200/80 fixed top-0 w-screen h-screen z-20 md:hidden transition-all duration-500 ease-in-out"
+                    v-if="sidebarOpen" @click="sidebarOpen = !sidebarOpen" />
+            </Transition>
+            <!-- Mobile Helper: background to close hamburger -->
+            <AppLeftSideBar class="-left-2/3 transition-all duration-300 ease-in-out z-20 block md:left-0"
+                :class="{ 'left-0': sidebarOpen }" @click="sidebarOpen = !sidebarOpen" />
+        </div>
 
-          <!-- Sidebar: Right -->
-          <AppRightSideBar class="fixed top-[76px] md:top-[68px] lg:top-16 w-44 transition-all duration-200 ease-in-out"
-              :class="[Object.values(layout.rightSidebar).some(value => value.show === true) ? 'right-0' : '-right-44']"
-          />
+        <!-- Main Content -->
+        <main
+            class="relative flex flex-col pt-20 pb-5 md:pt-[68px] lg:pt-16 bg-gray-50 text-gray-700 dark:bg-gray-700 dark:text-gray-400 transition-all duration-200 ease-in-out"
+            :class="[layoutState.leftSidebar.show ? 'ml-0 md:ml-56' : 'ml-0 md:ml-10']">
+            <slot />
+        </main>
 
-      </div>
+        <!-- Sidebar: Right -->
+        <AppRightSideBar class="fixed top-[76px] md:top-[68px] lg:top-16 w-44 transition-all duration-200 ease-in-out"
+            :class="[Object.values(layout.rightSidebar).some(value => value.show === true) ? 'right-0' : '-right-44']" />
 
-      <Footer  />
-      <notifications
-        dangerously-set-inner-html
-        :max="3"
-        :width="500"
-      />
+    </div>
 
-  </template>
+    <Footer />
+    <notifications dangerously-set-inner-html :max="3" :width="500" />
+</template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
