79ca7963e4168d562306af63bd918a6cae4727c8
Raul Perusquia <raul@inikoo.com>
2023-10-06 11:17:05 +0800

    remove webpage variants 🗑️

--- app/Models/Web/Webpage.php
+++ app/Models/Web/Webpage.php
@@ -15,59 +15,12 @@ use App\Http\Resources\Web\WebpageBlocksResource;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
-/**
- * App\Models\Web\Webpage
- *
- * @property int $id
- * @property string $slug
- * @property string $code
- * @property string $url
- * @property int $level
- * @property WebpageTypeEnum $type
- * @property WebpagePurposeEnum $purpose
- * @property int|null $parent_id
- * @property int $website_id
- * @property int|null $main_variant_id
- * @property array $content
- * @property array $blocks
- * @property array $compiled_content
- * @property array $data
- * @property array $settings
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $deleted_at
- * @property-read \App\Models\Web\WebpageStats|null $stats
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Web\WebpageVariant> $variants
- * @property-read int|null $variants_count
- * @property-read \App\Models\Web\Website $website
- * @method static Builder|Webpage newModelQuery()
- * @method static Builder|Webpage newQuery()
- * @method static Builder|Webpage query()
- * @method static Builder|Webpage whereBlocks($value)
- * @method static Builder|Webpage whereCode($value)
- * @method static Builder|Webpage whereCompiledContent($value)
- * @method static Builder|Webpage whereContent($value)
- * @method static Builder|Webpage whereCreatedAt($value)
- * @method static Builder|Webpage whereData($value)
- * @method static Builder|Webpage whereDeletedAt($value)
- * @method static Builder|Webpage whereId($value)
- * @method static Builder|Webpage whereLevel($value)
- * @method static Builder|Webpage whereMainVariantId($value)
- * @method static Builder|Webpage whereParentId($value)
- * @method static Builder|Webpage wherePurpose($value)
- * @method static Builder|Webpage whereSettings($value)
- * @method static Builder|Webpage whereSlug($value)
- * @method static Builder|Webpage whereType($value)
- * @method static Builder|Webpage whereUpdatedAt($value)
- * @method static Builder|Webpage whereUrl($value)
- * @method static Builder|Webpage whereWebsiteId($value)
- * @mixin \Eloquent
- */
+
 class Webpage extends Model
 {
     use HasSlug;
@@ -75,9 +28,7 @@ class Webpage extends Model
     protected $casts = [
         'data'             => 'array',
         'settings'         => 'array',
-        'blocks'           => 'array',
-        'content'          => 'array',
-        'compiled_content' => 'array',
+        'compiled_layout' => 'array',
         'type'             => WebpageTypeEnum::class,
         'purpose'          => WebpagePurposeEnum::class,
         'state'            => WebpageStateEnum::class,
@@ -87,9 +38,7 @@ class Webpage extends Model
     protected $attributes = [
         'data'             => '{}',
         'settings'         => '{}',
-        'blocks'           => '{}',
-        'content'          => '{}',
-        'compiled_content' => '{}',
+        'compiled_layout' => '{}',
     ];
 
     protected $guarded = [];
@@ -117,7 +66,11 @@ class Webpage extends Model
         return $this->belongsTo(Website::class);
     }
 
-    public function variants(): HasMany
+    public function snapshots(): MorphMany
+    {
+        return $this->morphMany(Snapshot::class, 'parent');
+    }
+    public function unpublishedSnapshot(): BelongsTo
     {
         return $this->hasMany(WebpageVariant::class);
     }
