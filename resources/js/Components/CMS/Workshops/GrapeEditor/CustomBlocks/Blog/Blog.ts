2245a69291c6dc3144041e9fba00309baec61c44
YudhistaA <aryarajasa0@gmail.com>
2023-10-12 08:08:49 +0800

    delete Block Old Contents

--- resources/js/Components/CMS/Workshops/GrapeEditor/CustomBlocks/Blog/Blog.ts
+++ resources/js/Components/CMS/Workshops/GrapeEditor/CustomBlocks/Blog/Blog.ts
@@ -5,11 +5,9 @@ export const BlogBlock1 = () => {
      category: 'Blog',
      label :  'Blog One',
      content : `
-     <section class="wowsbar-block-type-blog wowsbar-block">
-     <div class="container px-5 py-24 mx-auto">
-         <div class="flex m-4">
-             <!-- First Content Block -->
-             <div class="p-4 lg:w-1/3">
+     <section data-id="hero-block-1" data-type="html" data-group="hero" class="wowsbar-block container px-5 py-24 mx-auto"  data-gjs-editable="false" data-gjs-droppable="false">
+         <div class="flex m-4 flex-wrap lg:flex-nowrap">
+             <div class="p-4 lg:w-1/3 w-full">
                  <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                      <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
@@ -22,9 +20,8 @@ export const BlogBlock1 = () => {
                  </div>
              </div>
              
-             <!-- Second Content Block (similar structure) -->
 
-             <div class="p-4 lg:w-1/3">
+             <div class="p-4 lg:w-1/3 w-full">
              <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                  <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
@@ -37,9 +34,8 @@ export const BlogBlock1 = () => {
              </div>
          </div>
              
-             <!-- Third Content Block (similar structure) -->
 
-             <div class="p-4 lg:w-1/3">
+             <div class="p-4 lg:w-1/3 w-full">
              <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                  <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
@@ -50,10 +46,8 @@ export const BlogBlock1 = () => {
                      <span class="text-gray-400 inline-flex items-center leading-none text-sm"><svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path></svg>6</span>
                  </div>
              </div>
+         </div>   
          </div>
-             
-         </div>
-     </div>
  </section>
    `,
     }
--- resources/js/Components/CMS/Workshops/GrapeEditor/CustomBlocks/Hero/Hero.ts
+++ resources/js/Components/CMS/Workshops/GrapeEditor/CustomBlocks/Hero/Hero.ts
@@ -1,17 +1,12 @@
 export const heroBlock1 = () => {
     return {
-      disable: false,
-      activate: false,
-      category: 'Hero',
-      label:`Hero Theme One`,
-      id: "hero-block-1",
-      content : `
-    <section data-id="hero-block-1" data-type="html" data-group="hero" class="wowsbar-block" gjs-row"  data-gjs-editable="false" data-gjs-droppable="false">
-      <div class="relative isolate overflow-hidden bg-gray-900" data-gjs-droppable="false">
-        <div aria-hidden="true" class="absolute left--calc-50--4rem- top-10 -z-10 transform-gpu blur-3xl sm:left--calc-50--18rem- lg:left-48 lg:top--calc-50--30rem- xl:left--calc-50--24rem-">
-          <div class="aspect--1108-632- w--69-25rem- bg-gradient-to-r from--80caff- to--4f46e5- opacity-20" id="iqclm">
-          </div>
-        </div>
+        disable: false,
+        activate: false,
+        category: "Hero",
+        label: `Hero Theme One`,
+        id: "hero-block-1",
+        content: `
+    <section data-id="hero-block-1" data-type="html" data-group="hero" class="wowsbar-block relative isolate overflow-hidden bg-gray-900" gjs-row"  data-gjs-editable="false" data-gjs-droppable="false">
         <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
           <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
             <img src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="h-11"/>
@@ -36,28 +31,21 @@ export const heroBlock1 = () => {
             </div>
           </div>
         </div>
-      </div>
     </section>
-    `
-    }
+    `,
+    };
 };
 
-
 export const heroBlock2 = () => {
-  return {
-    disable: false,
-    activate: false,
-    category: 'Hero',
-    label:`Hero Theme Two`,
-    id: "hero-block-2",
-    content : `
+    return {
+        disable: false,
+        activate: false,
+        category: "Hero",
+        label: `Hero Theme Two`,
+        id: "hero-block-2",
+        content: `
   <section data-id="hero-block-2" data-type="html" data-group="hero" class="wowsbar-block gjs-row" data-gjs-editable="false">
-      <div class="relative isolate" data-gjs-droppable="false">
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true" data-gjs-droppable="false">
-          <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-        </div>
-        <div class="py-24 sm:py-32 lg:pb-40">
-          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+  <div class=" mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl text-center">
               <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
               <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
@@ -75,70 +63,50 @@ export const heroBlock2 = () => {
         </div>
         <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
           <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-        </div>
-      </div>
-    </div>
-  </section
-  `
-  }
+          </div>
+          </section
+  `,
+    };
 };
-
 
 export const heroBlock3 = () => {
-  return {
-    disable: false,
-    activate: false,
-    category: 'Hero',
-    label:`Hero Theme three`,
-    id: "hero-block-3",
-    content : `
-    <section data-id="hero-block-3" data-type="html" data-group="hero" class="wowsbar-block gjs-row"  data-gjs-editable="false">
-      <div class="bg-white" data-gjs-droppable="false">
-      <div class="relative" data-gjs-droppable="false">
-        <div class="mx-auto max-w-7xl">
-          <div class="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
-            <svg class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-              <polygon points="0,0 90,0 50,100 0,100" />
-            </svg>
-
-            <div class="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
-              <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                <div class="hidden sm:mb-10 sm:flex">
-                  <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. <a href="#" class="whitespace-nowrap font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-                  </div>
-                </div>
-                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-                <div class="mt-10 flex items-center gap-x-6">
-                  <a href="#" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                  <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
-                </div>
-              </div>
-            </div>
+    return {
+        disable: false,
+        activate: false,
+        category: "Hero",
+        label: `Hero Theme three`,
+        id: "hero-block-3",
+        content: `
+    <section data-id="hero-block-3" data-type="html" data-group="hero" class="wowsbar-block gjs-row relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14"  data-gjs-editable="false">
+    <div class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96" aria-hidden="true"></div>
+    <div class="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+        <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">We’re changing the way people connect.</h1>
+        <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+          <p class="text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</p>
+          <div class="mt-10 flex items-center gap-x-6">
+            <a href="#" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
           </div>
         </div>
-        <div class="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img class="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full" src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="">
-        </div>
+        <img src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80" alt="" class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36">
       </div>
     </div>
+    <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div>
   </section>
-  `
-  }
+  `,
+    };
 };
 
-
 export const heroBlock4 = () => {
-  return {
-    disable: false,
-    activate: false,
-    category: 'Hero',
-    label:`Hero Theme four`,
-    id: "hero-block-4",
-    content : `
-    <section data-id="hero-block-4" data-type="html" data-group="hero" class="wowsbar-block gjs-row"   data-gjs-editable="false">
-    <div class="text-gray-600 body-font" data-gjs-droppable="false">
+    return {
+        disable: false,
+        activate: false,
+        category: "Hero",
+        label: `Hero Theme four`,
+        id: "hero-block-4",
+        content: `
+    <section data-id="hero-block-4" data-type="html" data-group="hero" class="wowsbar-block gjs-row" data-gjs-editable="false">
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
                 <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
@@ -152,88 +120,23 @@ export const heroBlock4 = () => {
                 </div>
             </div>
         </div>
-    </div>
-  </section>`
-  }
+  </section>`,
+    };
 };
 
 export const heroBlock5 = () => {
-  return {
-    disable: false,
-    activate: false,
-    category: 'Hero',
-    label:`Hero Theme five`,
-    id: "hero-block-5",
-    content : `
-    <section data-id="hero-block-5" data-type="html" data-group="hero" class="wowsbar-block gjs-row"   data-gjs-editable="false">
-    <div class="bg-gray-50" data-gjs-droppable="false">
-  <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-    <div class="sm:flex sm:items-baseline sm:justify-between">
-      <h2 class="text-2xl font-bold tracking-tight text-gray-900">Shop by Category</h2>
-      <a href="#" class="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block">
-        Browse all categories
-        <span aria-hidden="true"> &rarr;</span>
-      </a>
+    return {
+        disable: false,
+        activate: false,
+        category: "Hero",
+        label: `Hero Theme five`,
+        id: "hero-block-5",
+        content: `
+    <section data-id="hero-block-5" data-type="html" data-group="hero" class="wowsbar-block gjs-row bg-white px-6 py-24 sm:py-32 lg:px-8"   data-gjs-editable="false">
+    <div class="mx-auto max-w-2xl text-center">
+      <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Support center</h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
     </div>
-
-    <div class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
-      <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
-        <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-featured-category.jpg" alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee." class="object-cover object-center group-hover:opacity-75">
-        <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50"></div>
-        <div class="flex items-end p-6">
-          <div>
-            <h3 class="font-semibold text-white">
-              <a href="#">
-                <span class="absolute inset-0"></span>
-                New Arrivals
-              </a>
-            </h3>
-            <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
-          </div>
-        </div>
-      </div>
-      <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full">
-        <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-01.jpg" alt="Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters." class="object-cover object-center group-hover:opacity-75 sm:absolute sm:inset-0 sm:h-full sm:w-full">
-        <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
-        <div class="flex items-end p-6 sm:absolute sm:inset-0">
-          <div>
-            <h3 class="font-semibold text-white">
-              <a href="#">
-                <span class="absolute inset-0"></span>
-                Accessories
-              </a>
-            </h3>
-            <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
-          </div>
-        </div>
-      </div>
-      <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full">
-        <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-02.jpg" alt="Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk." class="object-cover object-center group-hover:opacity-75 sm:absolute sm:inset-0 sm:h-full sm:w-full">
-        <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
-        <div class="flex items-end p-6 sm:absolute sm:inset-0">
-          <div>
-            <h3 class="font-semibold text-white">
-              <a href="#">
-                <span class="absolute inset-0"></span>
-                Workspace
-              </a>
-            </h3>
-            <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="mt-6 sm:hidden">
-      <a href="#" class="block text-sm font-semibold text-indigo-600 hover:text-indigo-500">
-        Browse all categories
-        <span aria-hidden="true"> &rarr;</span>
-      </a>
-    </div>
-  </div>
-</div>
-  </section>`
-  }
+  </section>`,
+    };
 };
-
-
