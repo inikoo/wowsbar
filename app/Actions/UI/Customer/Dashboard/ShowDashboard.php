ded74062891a28ccc89591b237935cb380473819
Raul Perusquia <raul@inikoo.com>
2023-10-17 08:20:55 +0800

    Customer dashboard 💠

--- app/Actions/UI/Customer/Dashboard/ShowDashboard.php
+++ app/Actions/UI/Customer/Dashboard/ShowDashboard.php
@@ -8,6 +8,7 @@
 namespace App\Actions\UI\Customer\Dashboard;
 
 use App\Actions\Portfolio\Banner\UI\GetLastEditedBanner;
+use App\Actions\Traits\WelcomeWidgets\WithFirstBanner;
 use Illuminate\Support\Arr;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -17,6 +18,7 @@ use Lorisleiva\Actions\Concerns\AsAction;
 class ShowDashboard
 {
     use AsAction;
+    use WithFirstBanner;
 
     public function asController(ActionRequest $request): Response
     {
@@ -32,63 +34,63 @@ class ShowDashboard
             'latest_banners_count'     => $latestBanners->count(),
             'portfolio_websites_count' => $customer->portfolioStats->number_portfolio_websites,
             'name'                     => $request->user()->contact_name ?? $request->user()->slug,
-
+            'firstBanner'              => $this->canEdit ? $this->getFirstBannerWidget($customer) : null,
         ];
 
 
         $welcomeStep = Arr::get($customer->data, 'welcome_step');
         if (in_array($welcomeStep, [1, 2, 3])) {
             $data['welcome'] = [
-                'currentStep'   => $welcomeStep,
-                'steps_data'    => [
-                    'step_1'    => [
+                'currentStep' => $welcomeStep,
+                'steps_data'  => [
+                    'step_1' => [
                         'id'                         => 1,
                         'label'                      => 'Enter your website name',
                         'description'                => 'Lorem ipsum dolor sit amet.',
                         'component'                  => 1,
                         'websiteValue'               => '',
                         'storePortfolioWebsiteRoute' => [
-                            'name'      => 'customer.models.portfolio-website.store.from-welcome'
+                            'name' => 'customer.models.portfolio-website.store.from-welcome'
                         ],
                     ],
-                    'step_2'    => [
-                        'id'                => 2,
-                        'label'             => 'Select your interest',
-                        'description'       => 'Lorem ipsum dolor sit amet.',
-                        'component'         => 2,
-                        "slug"              => "mw",
-                        "customer_name"     => "Aiku",
-                        "code"              => null,
-                        "name"              => "My website 😸",
-                        "url"               => "hello.com",
-                        "number_banners"    => 0,
-                        "seo"               => [
-                            "name"          => "seo",
-                            "label"         => "SEO",
-                            "value"         => 'not_sure'
+                    'step_2' => [
+                        'id'             => 2,
+                        'label'          => 'Select your interest',
+                        'description'    => 'Lorem ipsum dolor sit amet.',
+                        'component'      => 2,
+                        "slug"           => "mw",
+                        "customer_name"  => "Aiku",
+                        "code"           => null,
+                        "name"           => "My website 😸",
+                        "url"            => "hello.com",
+                        "number_banners" => 0,
+                        "seo"            => [
+                            "name"  => "seo",
+                            "label" => "SEO",
+                            "value" => 'not_sure'
                         ],
-                        "google-ads"    => [
-                            "name"          => "google-ads",
-                            "label"         => "PPC",
-                            "value"         => "not_interested"
+                        "google-ads"     => [
+                            "name"  => "google-ads",
+                            "label" => "PPC",
+                            "value" => "not_interested"
                         ],
-                        "social"        => [
-                            "name"          => "social",
-                            "label"         => "Social",
-                            "value"         => 'not_sure'
+                        "social"         => [
+                            "name"  => "social",
+                            "label" => "Social",
+                            "value" => 'not_sure'
                         ],
-                        "prospects"     => [
-                            "name"          => "prospects",
-                            "label"         => "Prospects",
-                            "value"         => 'not_sure'
+                        "prospects"      => [
+                            "name"  => "prospects",
+                            "label" => "Prospects",
+                            "value" => 'not_sure'
                         ],
-                        "banners"      => [
-                            "name"          => "banners",
-                            "label"         => "Banners",
-                            "value"         => 'not_sure'
+                        "banners"        => [
+                            "name"  => "banners",
+                            "label" => "Banners",
+                            "value" => 'not_sure'
                         ]
                     ],
-                    'step_3'    => [
+                    'step_3' => [
                         'id'            => 3,
                         'label'         => 'Make appointment',
                         'description'   => 'Lorem ipsum dolor sit amet.',
