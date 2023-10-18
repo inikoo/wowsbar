5c736d10e53510d1d1426e8af505c82471b9336f
Raul Perusquia <raul@inikoo.com>
2023-10-18 20:16:11 +0800

    Add sentry to the  🔭

--- resources/js/app-organisation.js
+++ resources/js/app-organisation.js
@@ -1,43 +1,57 @@
-import "./bootstrap";
-import "../css/app.css";
+import './bootstrap';
+import '../css/app.css';
 
-import { createApp, h } from "vue";
-import { createInertiaApp } from "@inertiajs/vue3";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
-import { i18nVue } from "laravel-vue-i18n"
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPlus } from '@far/';
-import Notifications from '@kyvg/vue3-notification'
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {i18nVue} from 'laravel-vue-i18n';
+import Notifications from '@kyvg/vue3-notification';
+import * as Sentry from '@sentry/vue';
 
-library.add(faPlus);
+import {createPinia} from 'pinia';
 
-import { createPinia } from "pinia";
 const appName =
-    window.document.getElementsByTagName("title")[0]?.innerText || "Wowsbar";
+          window.document.getElementsByTagName('title')[0]?.innerText ||
+          'Wowsbar';
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/Organisation/${name}.vue`,
-            import.meta.glob("./Pages/Organisation/**/*.vue")
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(createPinia())
-            .use(ZiggyVue, Ziggy)
-            .use(Notifications)
-            .use(i18nVue, {
-				resolve: async (lang) => {
-					const languages = import.meta.glob("../../lang/*.json")
-					return await languages[`../../lang/${lang}.json`]()
-				},
-			})
-            .mount(el);
-    },
-    progress: {
-        color: "#4B5563",
-    },
-});
+createInertiaApp(
+    {
+      title  : (title) => `${title} - ${appName}`,
+      resolve: (name) =>
+          resolvePageComponent(
+              `./Pages/Organisation/${name}.vue`,
+              import.meta.glob('./Pages/Organisation/**/*.vue'),
+          ),
+      setup({el, App, props, plugin}) {
+        const app = createApp({render: () => h(App, props)});
+
+        if(import.meta.env.VITE_SENTRY_ORG_DSN) {
+          Sentry.init({
+                        app,
+                        dsn                     : import.meta.env.VITE_SENTRY_ORG_DSN,
+                        environment             : import.meta.env.VITE_APP_ENV,
+                        integrations            : [
+                          new Sentry.Replay(),
+                        ],
+                        replaysSessionSampleRate: import.meta.env.VITE_SENTRY_ORG_REPLAY_SESSION_SAMPLE_RATE,
+                        replaysOnErrorSampleRate: import.meta.env.VITE_SENTRY_ORG_REPLAY_ON_ERRORSAMPLE_RATE,
+                      });
+        }
+        app.use(plugin).
+            use(createPinia()).
+            use(ZiggyVue, Ziggy).
+            use(Notifications).
+            use(i18nVue, {
+              resolve: async (lang) => {
+                const languages = import.meta.glob(
+                    '../../lang/*.json');
+                return await languages[`../../lang/${lang}.json`]();
+              },
+            }).mount(el);
+        return app;
+      },
+      progress: {
+        color: '#4B5563',
+      },
+    });
