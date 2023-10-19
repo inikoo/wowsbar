6f3efc80b47e464b9a5331244cae2007c6b6324e
artha <artha@aw-advantage.com>
2023-10-19 08:53:20 +0800

    social account icon

--- app/Enums/Portfolio/PortfolioSocialAccount/PortfolioSocialAccountPlatformEnum.php
+++ app/Enums/Portfolio/PortfolioSocialAccount/PortfolioSocialAccountPlatformEnum.php
@@ -16,4 +16,62 @@ enum PortfolioSocialAccountPlatformEnum: string
     case FACEBOOK         = 'facebook';
     case INSTAGRAM        = 'instagram';
     case TIKTOK           = 'tiktok';
+    case PINTEREST           = 'pinterest';
+    case LINKEDIN           = 'linkedin';
+    case TWITTER           = 'twitter';
+    case YOUTUBE           = 'youtube';
+
+    public static function platformIcon(): array
+    {
+        return [
+            self::FACEBOOK->value => [
+
+                'tooltip' => __(self::FACEBOOK->value),
+                'icon'    => 'fab fa-facebook',
+                'class'   => 'text-indigo-500'
+
+            ],
+            self::INSTAGRAM->value        => [
+
+                'tooltip' => __(self::INSTAGRAM->value),
+                'icon'    => 'fab fa-instagram'
+
+            ],
+            self::TIKTOK->value     => [
+
+                'tooltip' => __(self::TIKTOK->value),
+                'icon'    => 'fab fa-tiktok',
+                'class'   => 'text-black'
+
+            ],
+            self::PINTEREST->value     => [
+
+                'tooltip' => __(self::PINTEREST->value),
+                'icon'    => 'fab fa-pinterest',
+                'class'   => 'text-red-700'
+
+            ],
+            self::LINKEDIN->value     => [
+
+                'tooltip' => __(self::LINKEDIN->value),
+                'icon'    => 'fab fa-linkedin'
+
+            ],
+            self::TWITTER->value     => [
+
+                'tooltip' => __(self::TWITTER->value),
+                'icon'    => 'fab fa-twitter',
+                'class'   => 'text-sky-500'
+
+            ],
+            self::YOUTUBE->value     => [
+
+                'tooltip' => __(self::YOUTUBE->value),
+                'icon'    => 'fab fa-youtube',
+                'class'   => 'text-red-500'
+
+            ],
+
+        ];
+    }
 }
