4474f8e40d4030f57c979d863398fef0e3561fbb
aqordeon <aqordivika@yahoo.co.id>
2023-10-20 09:10:51 +0800

    BannerPreview: fix UI

--- resources/js/Components/Banner/BannerPreview.vue
+++ resources/js/Components/Banner/BannerPreview.vue
@@ -33,20 +33,29 @@ const props = defineProps<{
 
     <!-- If banner is 'square' -->
     <div v-else>
-        <div class="w-fit bg-white flex flex-col md:flex-row items-center justify-between py-3 px-4 gap-y-1">
+        <div class="bg-white flex flex-col md:flex-row items-center justify-between py-3 px-4 gap-y-1 gap-x-1">
             <!-- Title -->
-            <div class="flex gap-x-2">
+            <div v-if="data.published_snapshot.publisher_avatar || data.published_snapshot.publisher || data.published_snapshot.comment"
+                class="flex gap-x-2"
+            >
                 <div class="h-5 aspect-square rounded-full overflow-hidden ring-1 ring-gray-300">
                     <Image :src="data.published_snapshot.publisher_avatar" />
                 </div>
-                <div class="font-bold text-lg leading-none">{{ data.published_snapshot.publisher }}</div>
+                <div class="font-bold text-base md:text-lg leading-none">{{ data.published_snapshot.publisher }}</div>
                 <div v-if="data.published_snapshot.comment" class="text-sm text-gray-500 italic">
                     ({{ data.published_snapshot.comment }})
                 </div>
             </div>
+            
+            <div v-else class="text-gray-600 text-xxs md:text-sm">
+                This banner is not published yet.
+            </div>
+
             <!-- Published at -->
-            <div class="text-sm italic text-gray-500 tracking-wide text-right font-light">Published at <span
-                    class="font-bold">{{ useRangeFromNow(data.published_snapshot.published_at) }}</span> ago</div>
+            <div v-if="data.published_snapshot.published_at" class="text-xs md:text-sm italic text-gray-500 tracking-wide text-right font-light">
+                Published at
+                <span class="font-bold">{{ useRangeFromNow(data.published_snapshot.published_at) }}</span> ago
+            </div>
         </div>
         <div class="flex flex-col h-48 lg:h-64 xl:h-96 w-fit ">
             <Slider :data="data.compiled_layout" :production="false" />
