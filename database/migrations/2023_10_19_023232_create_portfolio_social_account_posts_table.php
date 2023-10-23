d2ddd90c26f8d8fbfa9050b4063d8a45bc6f5b1e
Raul Perusquia <raul@inikoo.com>
2023-10-23 13:55:22 +0800

    add soft deletes

--- database/migrations/2023_10_19_023232_create_portfolio_social_account_posts_table.php
+++ database/migrations/2023_10_19_023232_create_portfolio_social_account_posts_table.php
@@ -1,26 +1,29 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 23 Oct 2023 13:35:17 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
 
 use App\Enums\Portfolio\PortfolioSocialAccount\PortfolioSocialAccountPostStatusEnum;
 use App\Enums\Portfolio\PortfolioSocialAccount\PortfolioSocialAccountPostTypeEnum;
+use App\Stubs\Migrations\HasSoftDeletes;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
+    use HasSoftDeletes;
+
     public function up(): void
     {
         Schema::create('portfolio_social_account_posts', function (Blueprint $table) {
-            $table->id();
-
+            $table->increments('id');
             $table->string('task_name');
             $table->string('slug');
             $table->dateTimeTz('start_at');
             $table->dateTimeTz('end_at')->nullable();
             $table->string('duration')->default(0);
-
             $table->unsignedBigInteger('portfolio_social_account_id');
             $table->foreign('portfolio_social_account_id')->references('id')->on('portfolio_social_accounts')->onDelete('cascade');
 
@@ -29,14 +32,11 @@ return new class () extends Migration {
 
             $table->string('description')->nullable();
             $table->string('notes')->nullable();
-
+            $table = $this->softDeletes($table);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('portfolio_social_account_posts');
