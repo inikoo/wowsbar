201a097bf0074e0298fcddf179a9e2e89d26fe0d
YudhistaA <aryarajasa0@gmail.com>
2023-11-02 17:00:26 +0800

    Set Ready Email Workshop

--- resources/js/Pages/Organisation/CRM/Prospects/WorkshopMailshot.vue
+++ resources/js/Pages/Organisation/CRM/Prospects/WorkshopMailshot.vue
@@ -12,11 +12,18 @@ import Publish from '@/Components/Utils/Publish.vue'
 import { capitalize } from "@/Composables/capitalize"
 import { ref, computed } from "vue"
 import { faSign, faGlobe, faPencil, faSeedling, faPaste, faLayerGroup } from '@fal/'
+import { faCaretRight } from '@fas/'
 import MailshotWorkshopComponent from "@/Components/Workshop/MailshotWorkshopComponent.vue";
 import axios from 'axios'
 import { cloneDeep } from 'lodash'
+import Button from '@/Components/Elements/Buttons/Button.vue';
+import ButtonGroup from '@/Components/Elements/Buttons/ButtonGroup.vue'
+import Popover from '@/Components/Utils/Popover.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Modal from '@/Components/Utils/Modal.vue';
+import PureDatePicker from '@/Components/Pure/PureDatePicker.vue'
 
-library.add(faSign, faGlobe, faPencil, faSeedling, faPaste, faLayerGroup)
+library.add(faSign, faGlobe, faPencil, faSeedling, faPaste, faLayerGroup, faCaretRight)
 
 const props = defineProps<{
     title: string,
@@ -34,33 +41,25 @@ const props = defineProps<{
 
 }>()
 
-const isLoading = ref(false)
-const comment = ref('')
+const OpenModal = ref(false)
+const date = ref(new Date())
 
-const isDataDirty = ref(cloneDeep(props.isDirty))
 
 const sendDataToServer = async () => {
-    isLoading.value = true
+    console.log(date.value.toISOString())
     try {
         const response = await axios.post(
             route(
                 props.setAsReadyRoute.name,
                 props.setAsReadyRoute.parameters
             ),
-            { comment: comment.value },
+            { schedule: date.value.toISOString() },
         )
         console.log('publish......')
-        comment.value = ''
     } catch (error) {
-        comment.value = ''
         console.log(error)
     }
-    isLoading.value = false
 }
-
-const compIsDataFirstTimeCreated = computed(() => {
-    return false
-})
 
 </script>
 
@@ -69,11 +68,47 @@ const compIsDataFirstTimeCreated = computed(() => {
     <Head :title="capitalize(title)" />
     <PageHeading :data="pageHead">
         <template #other="{ dataPageHead: head }">
-            <Publish v-model="comment" :isDataFirstTimeCreated="compIsDataFirstTimeCreated" :isHashSame="!isDataDirty"
-                :isLoading="isLoading" :saveFunction="sendDataToServer" :firstPublish="websiteState != 'live'" />
+            <div class="flex">
+                <Button @click="sendDataToServer"
+                    class="relative capitalize items-center rounded-none text-sm border-none font-medium shadow-sm focus:ring-transparent focus:ring-offset-transparent focus:ring-0">
+                    Send Now
+                </Button>
+                <Popover>
+                    <template #button>
+                        <Button 
+                            class="capitalize inline-flex items-center h-full rounded-none text-sm border-none font-medium shadow-sm focus:ring-transparent focus:ring-offset-transparent focus:ring-0" style="padding:9px;">
+                            <font-awesome-icon :icon="['fas', 'caret-right']" />
+                        </Button>
+                    </template>
+                    <template #content>
+                       <div @click="OpenModal = true">Send with schedule</div> 
+                    </template>
+
+                </Popover>
+
+
+            </div>
         </template>
     </PageHeading>
     <MailshotWorkshopComponent :useBasic="false" :imagesUploadRoute="imagesUploadRoute" :updateRoute="updateRoute"
         :loadRoute="loadRoute" />
+
+        <Modal :isOpen="OpenModal" @onClose="OpenModal = false" width="w-fit">
+        <div>
+        <div class="m-2"><VDatePicker v-model="date" mode="dateTime" is24hr /></div>
+            <div class="flex justify-between">
+            <div class="p-[4px]">Cancel</div>
+            <Button @click="sendDataToServer">Schedule</Button>
+            </div>
+        </div>
+     
+        </Modal>
 </template>
 
+<style>
+.vc-time-select-group select {
+    background: transparent;
+    padding: 0px 4px;
+    border: none;
+}
+</style>
