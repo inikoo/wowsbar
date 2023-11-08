2aaba9a411706994a1dbfd03883d0f52b1517463
aqordeon <aqordivika@yahoo.co.id>
2023-11-08 17:02:11 +0800

    fix: Public Appointment calendar

--- resources/js/Components/Blocks/Appointment.vue
+++ resources/js/Components/Blocks/Appointment.vue
@@ -1,158 +1,232 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from 'vue';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faRocketLaunch, faClock, faVideo } from "@far/";
 import { isNull } from "lodash";
 import { useFormatTime } from "@/Composables/useFormatTime";
-import { Calendar } from 'v-calendar';
+import { DatePicker } from 'v-calendar';
 import 'v-calendar/style.css';
+import axios from 'axios'
+import Button from '@/Components/Elements/Buttons/Button.vue';
+import { notify } from '@kyvg/vue3-notification';
 
 library.add(faRocketLaunch, faClock, faVideo);
 
 const props = defineProps<{
-    data: Object;
-}>();
-console.log("ini", props.data);
-console.log("porpsd", props);
+    data: {
+        info: string
+        title: string
+    }
+}>()
 
-const Book = {
-    description:
-        "The Basic tee is an honest new take on a classic. The tee uses super soft, pre-shrunk cotton for true comfort and a dependable fit",
-    meet: {
-        customerService: "Arya",
-        duration: "30 mnt",
-    },
-    title: "Discovery Call - Wowsbar",
-    meetInformation: "Web conferencing details provided upon confirmation.",
-};
+const unavailableDates: any = ref([])
+const availableSchedules = ref({})
+const selectedDated: any = ref()
+
+// console.log("ini", props.data);
+// console.log("porpsd", props);
+
+
 const attrs = ref([
     {
         key: "today",
         highlight: {
-            color: "purple",
+            color: "gray",
             fillMode: "outline",
         },
         dates: new Date(),
-    },
-    {
-        key: "tgl2",
-        highlight: {
-            color: "purple",
-            fillMode: "outline",
-        },
-        dates: new Date(2023, 8, 2),
-    },
-    {
-        key: "tgl7",
-        highlight: {
-            color: "purple",
-            fillMode: "outline",
-        },
-        dates: new Date(2023, 8, 4),
-    },
-    {
-        key: "tgl6",
-        highlight: {
-            color: "purple",
-            fillMode: "outline",
-        },
-        dates: new Date(2023, 8, 8),
-    },
-    {
-        key: "tgl4",
-        highlight: {
-            color: "purple",
-            fillMode: "outline",
-        },
-        dates: new Date(2023, 8, 20),
-    },
-]);
+    }
+])
 
 const hours = [
-    "07.00 Am",
-    "08.00 Am",
-    "09.00 Am",
-    "10.00 Am",
-    "11.00 Am",
-    "12.00 Am",
-    "13.00 Am",
-];
-
-const selectedDate = ref(0);
-
-const handleDateClick = (date) => {
-    if (date.attributes.length > 0) {
-        if (!isNull(selectedDate.value)) {
-            attrs.value[selectedDate.value].highlight.fillMode = "outline";
-        }
-        const index = attrs.value.findIndex(
-            (item) => date.attributes[0].key == item.key
-        );
-        if (index >= 0) {
-            attrs.value[index].highlight.fillMode = "solid";
-            selectedDate.value = index;
-        }
+    {
+        value: 7,
+        label: '7 am'
+    },
+    {
+        value: 8,
+        label: '8 am'
+    },
+    {
+        value: 9,
+        label: '9 am'
+    },
+    {
+        value: 10,
+        label: '10 am'
+    },
+    {
+        value: 11,
+        label: '11 am'
+    },
+    {
+        value: 12,
+        label: '12 pm'
+    },
+    {
+        value: 13,
+        label: '1 pm'
+    },
+    {
+        value: 14,
+        label: '2 pm'
+    },
+    {
+        value: 15,
+        label: '3 pm'
+    },
+    {
+        value: 16,
+        label: '4 pm'
+    },
+    {
+        value: 17,
+        label: '5 pm'
     }
-};
+]
 
-const getDate = () => {
-    if (!isNull(selectedDate.value)) {
-        return useFormatTime(attrs.value[selectedDate.value].dates);
+
+const onSelectHour = (hour: number) => {
+    selectedDated.value = new Date(selectedDated.value)
+    selectedDated.value.setHours(hour)
+}
+
+onMounted(async () => {
+    try {
+        const response = await axios.get(
+            route('public.appointment.schedule'),
+            {
+                params: {
+                    year: '2023',
+                    month: '11'
+                }
+            }
+        )
+        console.log('xxx', response.data)
+        // unavailableDates.value = Object.keys(response.data.bookedSchedules).map((item) => {
+        //     return new Date(item)
+        // })
+
+        unavailableDates.value = response.data.bookedSchedules
+        availableSchedules.value = response.data.availableSchedules
+        // console.log('response', unavailableDates.value)
     }
-    return ""; // Handle when no date is selected
-};
+    catch (error: any) {
+        console.log('error', error)
+    }
+
+})
+
+const isHourAvailable = (hour: number) => {
+    if (selectedDated.value.length == false) return true  // If not selected date yet
+
+
+    const formatUnavailable = unavailableDates.value[getDateOnly(selectedDated.value)] ? unavailableDates.value[getDateOnly(selectedDated.value)].map(item => new Date(item)) : []
+
+    return !formatUnavailable.map(item => item.getHours() == hour)[0]
+
+}
+
+const getDateOnly = (dateString: string) => {
+    const date = new Date(dateString);
+
+    // Extract the year, month, and day components
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // Month is zero-based, so add 1
+    const day = date.getDate();
+
+    // Create the desired date string 'YYYY-MM-DD'
+    const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    return formattedDate
+}
+
+const onClickMakeAppointment = async () => {
+    console.log('Appointment created')
+    try {
+        const response = await axios.get(
+            route('public.appointment.schedule'),
+            {
+                params: {
+                    year: '2023',
+                    month: '11'
+                }
+            }
+        )
+
+        notify({
+            title: "Appointment successfuly created.",
+            // text: error,
+            type: "success"
+        });
+    }
+    catch (error: any) {
+        notify({
+            // title: "Appointment successfuly created.",
+            text: error,
+            type: "error"
+        });
+    }
+}
+
 </script>
-<template>
-    <div style="margin: 20px;" >
-        <div class="container border-2" style="max-width: 800px;">
-        <div class="row">
-            <div class="col-md-6 p-0">
-                <div class="card text-center rounded-0 border-0">
-                    <div
-                        class="card-body"
 
-                    >
-                        <div v-html="data.info"></div>
+
+<template>
+    <!-- <pre>{{ unavailableDates[getDateOnly(selectedDated)] }}</pre> -->
+    <!-- {{ selectedDated }} -->
+    <div style="margin: 20px;">
+        <div class="container border-2" style="max-width: 800px;">
+            <div class="row divide-x divide-gray-300">
+                <!-- Section: Left (Blog) -->
+                <div class="col-md-6 p-0">
+                    <div class="text-center rounded-0 border-0">
+                        <div class="card-body">
+                            <div v-html="data.info"></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 p-0">
-                <div class="card text-center rounded-0 border-0">
-                    <div class="card-body" style="border-left: 1px solid #d1d5db; height:100%">
+                
+                <!-- Section: Right (calendar) -->
+                <div class="col-md-6 p-0 text-center">
+                    <div class="card-body">
                         <div class="bg-white rounded text-left">
                             <span v-html="data.title"></span>
                         </div>
                         <div>
-                            <Calendar
-                                expanded
-                                :attributes="attrs"
-                                @dayclick="handleDateClick"
-                            />
+                            <DatePicker v-model="selectedDated" expanded :attributes="attrs" />
                         </div>
-                        <div>
-                            <div v-if="!isNull(selectedDate)">
-                                <div class="px-2.5">{{ getDate() }}</div>
-                                <div class="px-2.5 my-4">
-                                    <div
-                                        class="flex flex-wrap justify-start gap-3"
+
+                        <div class="px-2.5 my-4 space-y-4">
+                            <div v-if="selectedDated" class="grid grid-cols-3 justify-center gap-3">
+                                <div v-if="availableSchedules[getDateOnly(selectedDated)]" v-for="hour in availableSchedules[getDateOnly(selectedDated)]" class="w-full">
+                                    <Button
+                                        :key="selectedDated + hour"
+                                        @click="onSelectHour(hour.split(':')[0])" full
+                                        :style="isHourAvailable(hour.split(':')[0])
+                                            ? new Date(selectedDated).getHours() == hour.split(':')[0]
+                                                ? 'rainbow'  // If hour selected
+                                                : 'secondary'  // If available but not selected
+                                            : 'disabled'  // If unavailable
+                                            "
                                     >
-                                        <button
-                                            v-for="hour in hours"
-                                            :key="hour"
-                                            @click="selectHour(hour)"
-                                            class="rounded-md border border-transparent bg-indigo-600 px-2 py-1 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                        >
-                                            {{ hour }}
-                                        </button>
-                                    </div>
+                                        {{ hour }}
+                                    </Button>
+                                </div>
+                                <div v-else class="col-span-3">No schedules available</div>
+                                <div v-if="hours.some(hour => hour.value == new Date(selectedDated).getHours())" class="col-span-3">
+                                    <Button @click="onClickMakeAppointment()" label="Make appointment" full />
                                 </div>
                             </div>
+
+                            <!-- If not selected date yet -->
+                            <div v-else class="text-gray-500 italic">
+                                ---- Select date to make an appointment ----
+                            </div>
+                            <!-- {{ selectedDated }} -->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-
 </template>
