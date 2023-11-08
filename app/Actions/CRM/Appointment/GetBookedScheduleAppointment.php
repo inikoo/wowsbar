f2c553f8cce911eaf1cfbce57646729c84d3d320
artha <artha@aw-advantage.com>
2023-11-08 13:09:18 +0800

    PKA-1234 Continue Appointment

--- app/Actions/CRM/Appointment/GetBookedScheduleAppointment.php
+++ app/Actions/CRM/Appointment/GetBookedScheduleAppointment.php
@@ -7,20 +7,17 @@
 
 namespace App\Actions\CRM\Appointment;
 
-use App\Enums\CRM\Appointment\AppointmentEventEnum;
-use App\Enums\CRM\Appointment\AppointmentTypeEnum;
 use App\Models\CRM\Appointment;
 use App\Models\Market\Shop;
 use Exception;
 use Illuminate\Console\Command;
-use Illuminate\Support\Collection;
-use Illuminate\Validation\Rule;
+use Illuminate\Support\Carbon;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
 use Throwable;
 
-class GetScheduleAppointment
+class GetBookedScheduleAppointment
 {
     use AsAction;
     use WithAttributes;
@@ -29,9 +26,24 @@ class GetScheduleAppointment
     /**
      * @throws Throwable
      */
-    public function handle(string $date): Collection
+    public function handle(array $modelData): array
     {
-        return Appointment::whereDate('schedule_at', $date)->get();
+        $dt = Carbon::createFromDate($modelData['year'], $modelData['month']);
+        $bookedSchedules = [];
+
+        for ($i = 1; $i <= $dt->daysInMonth; $i++) {
+            $date = $dt->format('Y-m-d');
+
+            $appointments = Appointment::whereDate('schedule_at', $date)->get();
+
+            foreach ($appointments as $appointment) {
+                $bookedSchedules[$date][] = $appointment->schedule_at;
+            }
+        }
+
+        return [
+            'bookedSchedules' => $bookedSchedules
+        ];
     }
 
     public function authorize(ActionRequest $request): bool
@@ -42,20 +54,19 @@ class GetScheduleAppointment
     public function rules(): array
     {
         return [
-            'schedule_at'              => ['required', 'string'],
-            'description'              => ['nullable', 'string', 'max:255'],
-            'type'                     => ['required', Rule::in(AppointmentTypeEnum::values())],
-            'event'                    => ['required', Rule::in(AppointmentEventEnum::values())],
-            'event_address'            => ['required', 'string']
+            'year'  => ['required', 'string'],
+            'month' => ['required', 'string']
         ];
     }
 
     /**
      * @throws Throwable
      */
-    public function asController(ActionRequest $request): Collection
+    public function asController(ActionRequest $request): array
     {
-        return $this->handle($request->input('date'));
+        $request->validate();
+
+        return $this->handle($request->validated());
     }
 
     public string $commandSignature = 'shop:new-customer {shop} {email} {--N|contact_name=} {--C|company=} {--P|password=}';
