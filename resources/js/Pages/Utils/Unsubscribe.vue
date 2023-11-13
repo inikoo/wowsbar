7400240fc3d1f5181fea6c81454420c1a096a0b2
aqordeon <aqordivika@yahoo.co.id>
2023-11-13 14:43:05 +0800

    feat: Unsubscribe page

---
+++ resources/js/Pages/Utils/Unsubscribe.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faFrown } from '@fal/'
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faFrown)
+</script>
+
+<template>
+    <div class="py-16 sm:py-20">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 text-gray-600 ">
+            <div class="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-50 to-gray-100 relative isolate overflow-hidden px-6 py-24 shadow-xl border-2 border-violet-100 sm:rounded-2xl sm:px-24 xl:py-32">
+                <div class="w-full text-center">
+                    <FontAwesomeIcon icon='fal fa-frown' class='h-10' aria-hidden='true' />
+                </div>
+                <h2 class="mx-auto max-w-2xl text-center text-3xl font-semibold tracking-tight sm:text-4xl">
+                    Unsubscribe successfully.
+                </h2>
+                <p class="mx-auto mt-2 max-w-2xl text-center text-lg leading-8 text-gray-500">
+                    We're sorry to see you go. If you unsubscribe by mistakes, you can resubscribe 'here'.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
