36ee7f240f5b2020873b21261aaa1813ddbcec4a
Raul Perusquia <raul@inikoo.com>
2023-11-14 17:10:20 +0800

    Fix WOWSBAR-3S dont use ES for retreaving banner

--- app/Actions/Portfolio/Banner/UI/DeliverBanner.php
+++ app/Actions/Portfolio/Banner/UI/DeliverBanner.php
@@ -7,8 +7,8 @@
 
 namespace App\Actions\Portfolio\Banner\UI;
 
-use App\Actions\Elasticsearch\BuildElasticsearchClient;
-use Illuminate\Support\Arr;
+use App\Models\Portfolio\Banner;
+use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 use Inertia\Response;
 use Lorisleiva\Actions\Concerns\AsController;
@@ -19,27 +19,29 @@ class DeliverBanner
 
     public function handle(string $ulid): array
     {
-
-
+        /*
         $client = BuildElasticsearchClient::run();
-
         $params = [
             'index' => config('elasticsearch.index_prefix').'content_blocks',
             'id'    => 'banner_'.$ulid
         ];
-
-
         $response = $client->get($params)->asString();
+        $bannerSource=Arr::get(json_decode($response, true), '_source');
+                return $bannerSource;
+*/
 
+        $seconds=3600;
 
+        return Cache::remember('deliver_banner_compiled_layout_'.$ulid, $seconds, function () use ($ulid) {
+            $banner = Banner::where('ulid', $ulid)->firstOrFail();
+            return $banner->compiled_layout;
+        });
 
-        return Arr::get(json_decode($response, true), '_source');
     }
 
 
     public function htmlResponse(array $compiledLayout): Response
     {
-
         return Inertia::render(
             'Banner',
             [
