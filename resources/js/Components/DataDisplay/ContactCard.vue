232e16f5c5ec2d7c09eed5f80e960c42519cd12d
aqordeon <aqordivika@yahoo.co.id>
2023-11-16 12:01:51 +0800

    fix: update timeline UI ProspectShowcase

--- resources/js/Components/DataDisplay/ContactCard.vue
+++ resources/js/Components/DataDisplay/ContactCard.vue
@@ -4,6 +4,7 @@ import { faUser, faCalendarAlt, faEnvelope, faPhone, faGlobe } from '@fal/'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import Tag from '@/Components/Tag.vue';
 import { useFormatTime } from '../../Composables/useFormatTime';
+import CopyButton from '@/Components/Utils/CopyButton.vue';
 library.add(faUser, faCalendarAlt, faEnvelope, faPhone, faGlobe)
 
 const props = defineProps<{
@@ -25,7 +26,7 @@ const props = defineProps<{
             <div class="flex justify-between items-end w-full px-6 py-6">
                 <div class="flex flex-col items-end">
                     <!-- <dt class="text-sm font-semibold leading-6">Amount</dt> -->
-                    <dd class="mt-1 text-lg font-semibold whitespace-nowrap">{{ data.name }}</dd>
+                    <dd class="bg-gradient-to-br from-slate-900 via-purple-800 to-slate-900 bg-clip-text text-transparent mt-1 text-lg font-semibold whitespace-nowrap">{{ data.name }}</dd>
                 </div>
 
                 <!-- Section: Tag -->
@@ -39,36 +40,44 @@ const props = defineProps<{
 
             <!-- Section: Contact Information -->
             <div class="flex flex-col w-full py-2 text-gray-500">
+                <!-- Info: Date -->
                 <div class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
-                        <span class="sr-only">User</span>
+                        <span class="sr-only">Date</span>
                         <FontAwesomeIcon icon='fal fa-calendar-alt' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ useFormatTime(data.date) }}</dd>
+                    <CopyButton v-if="data.date" :text="useFormatTime(data.date)" />
                 </div>
                 
+                <!-- Info: Email -->
                 <div class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
-                        <span class="sr-only">User</span>
+                        <span class="sr-only">Email</span>
                         <FontAwesomeIcon icon='fal fa-envelope' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ data.email ?? '-' }}</dd>
+                    <CopyButton v-if="data.email" :text="data.email" />
                 </div>
 
+                <!-- Info: Phone -->
                 <div class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
-                        <span class="sr-only">User</span>
+                        <span class="sr-only">Phone</span>
                         <FontAwesomeIcon icon='fal fa-phone' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ data.phone ?? '-' }}</dd>
+                    <CopyButton v-if="data.phone" :text="data.phone" />
                 </div>
 
+                <!-- Info: Website -->
                 <div class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
-                        <span class="sr-only">User</span>
+                        <span class="sr-only">Website</span>
                         <FontAwesomeIcon icon='fal fa-globe' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ data.website ?? '-' }}</dd>
+                    <CopyButton v-if="data.website" :text="data.website" />
                 </div>
 
             </div>
--- resources/js/Pages/Organisation/Prospects/ProspectShowcase.vue
+++ resources/js/Pages/Organisation/Prospects/ProspectShowcase.vue
@@ -5,30 +5,13 @@ import Image from '@/Components/Image.vue'
 import { useCopyText } from '@/Composables/useCopyText'
 import { useFormatTime } from '@/Composables/useFormatTime'
 import ContactCard from '@/Components/DataDisplay/ContactCard.vue'
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPhone, faEnvelope, faGlobe, faUser } from '@fal/'
-import { library } from '@fortawesome/fontawesome-svg-core'
 import Tag from '@/Components/Tag.vue';
 import Timeline from '@/Components/Utils/Timeline.vue'
-library.add(faPhone, faEnvelope, faGlobe, faUser)
+import Feeds from '@/Components/Utils/Feeds.vue'
 
 const props = defineProps<{
     data: any
 }>()
-
-const timelineSort = props.data.timeline.sort((a, b) => {
-    return a.value - b.value;
-})
-
-const timelineFilterNull = Object.groupBy(timelineSort.filter(obj => obj.value), (item) => {
-    return item.value
-})
-
-const titlesObject = {};
-for (const key in timelineFilterNull) {
-  titlesObject[key] = timelineFilterNull[key].map(item => item.title);
-}
 
 const dataContact = {
     name: props.data.info.name,
@@ -38,13 +21,34 @@ const dataContact = {
     website: props.data.info.website,
     tags: props.data.info.tags
 }
+
+const dataFeeds = [
+    { name: 'Chelsea Hagon', action: 'created', dateTime: '2023-01-23T10:32' },
+    { name: 'Chelsea Hagon', action: 'updated', dateTime: '2023-01-23T11:03' },
+    { name: 'Chelsea Hagon', action: 'deleted', dateTime: '2023-01-23T11:24' },
+    {
+        name: 'Chelsea Hagon',
+        action: 'commented',
+        comment: 'Called client, they reassured me the invoice would be paid by the 25th.',
+        dateTime: '2023-01-23T15:56',
+    },
+    { name: 'Alex Curren', action: 'updated', dateTime: '2023-01-24T09:12' },
+    { name: 'Alex Curren', action: 'published', dateTime: '2023-01-24T09:20' },
+]
+
 </script>
 
 <template>
-    <!-- <pre>{{ data.info }}</pre> -->
-    <Timeline :options="titlesObject"/>
+    <div class="grid grid-cols-8 px-4 py-4">
+        <!-- Section: Contact Card -->
+        <div class="col-span-5 space-y-8">
+            <ContactCard :data="dataContact"/>
+        </div>
 
-    <div class="px-4 py-4 space-y-8">
-        <ContactCard :data="dataContact"/>
+        <!-- Section: Feeds -->
+        <div class="col-span-3 py-3 pl-4 pr-8 rounded-md border border-gray-200 shadow">
+            <div class="font-semibold text-lg mb-4">{{ trans("Recent activity") }}</div>
+            <Feeds :dataFeeds="dataFeeds" />
+        </div>
     </div>
 </template>
