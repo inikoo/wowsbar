b07b55659f157061a18212a9238215d5b00cd2a1
Raul Perusquia <raul@inikoo.com>
2023-11-20 12:13:44 +0800

    fix: prospect list with 0 result filtering 🐛

--- app/Actions/Leads/Prospect/UI/IndexProspects.php
+++ app/Actions/Leads/Prospect/UI/IndexProspects.php
@@ -142,6 +142,8 @@ class IndexProspects extends InertiaAction
                     );
                 }
             }
+
+
             $table
                 ->withModelOperations($modelOperations)
                 ->withGlobalSearch()
@@ -149,7 +151,7 @@ class IndexProspects extends InertiaAction
                     [
                         'title'       => __('no prospects'),
                         'description' => null,
-                        'count'       => 0
+                        'count'       => $parent->crmStats->number_prospects
                     ]
                 )
                 ->column(key: 'state', label: ['fal', 'fa-yin-yang'], type: 'icon')
