9155d356a5712595c19a9bd9c5f976f8e07dd83a
aqordeon <aqordivika@yahoo.co.id>
2023-11-22 13:39:31 +0800

    fix: WW-53 UI: Unsubscribe Email

--- resources/js/Pages/Public/Utils/Unsubscribe.vue
+++ resources/js/Pages/Public/Utils/Unsubscribe.vue
@@ -1,9 +1,9 @@
 <script setup lang='ts'>
 import { Head } from '@inertiajs/vue3'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFrown } from '@fal/'
+import { faCheckCircle } from '@fas/'
 import { library } from '@fortawesome/fontawesome-svg-core'
-library.add(faFrown)
+library.add(faCheckCircle)
 
 const props = defineProps<{
     message: {
@@ -19,18 +19,18 @@ const props = defineProps<{
     <div class="py-16 sm:py-20">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 text-gray-600 ">
             <div class="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-50 to-gray-50 relative isolate overflow-hidden px-6 py-24 shadow-xl border-2 border-violet-100 sm:rounded-2xl sm:px-24 xl:py-32">
-                <div class="w-full text-center">
-                    <FontAwesomeIcon icon='fal fa-frown' class='h-10' aria-hidden='true' />
-                </div>
                 <h2 class="mx-auto max-w-2xl text-center text-3xl font-semibold tracking-tight sm:text-4xl">
-                    {{ message.title }}
+                    {{ message?.title ?? 'You have been unsubscribed' }}
                 </h2>
                 <p class="mx-auto mt-2 max-w-2xl text-center text-lg leading-8 text-gray-500">
-                    {{ message.description }}
+                    {{ message?.description }}
                 </p>
                 <p class="mx-auto mt-2 max-w-2xl text-center text-md leading-8 text-red-500 font-bold">
-                    {{ message.caution }}
+                    {{ message?.caution }}
                 </p>
+                <div class="w-full text-center">
+                    <FontAwesomeIcon icon='fas fa-check-circle' class='h-10 text-lime-400' aria-hidden='true' />
+                </div>
             </div>
         </div>
     </div>
