b4cd6e02eecc5f65823fe9e7ad1efb7d88ad3724
aqordeon <aqordivika@yahoo.co.id>
2023-11-22 16:40:28 +0800

    fix: WW-57 Prospect Showcase hide row when not exist

--- resources/js/Components/DataDisplay/ContactCard.vue
+++ resources/js/Components/DataDisplay/ContactCard.vue
@@ -43,43 +43,43 @@ const props = defineProps<{
             <!-- Section: Contact Information -->
             <div class="flex flex-col w-full py-2 text-gray-500">
                 <!-- Info: Date -->
-                <div class="flex items-center w-full gap-x-4 px-6 py-3">
+                <div v-if="data.date" class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
                         <span class="sr-only">Date</span>
                         <FontAwesomeIcon icon='fal fa-calendar-alt' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ useFormatTime(data.date) }}</dd>
-                    <CopyButton v-if="data.date" :text="useFormatTime(data.date)" />
+                    <CopyButton :text="useFormatTime(data.date)" />
                 </div>
                 
                 <!-- Info: Email -->
-                <div class="flex items-center w-full gap-x-4 px-6 py-3">
+                <div v-if="data.email" class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
                         <span class="sr-only">Email</span>
                         <FontAwesomeIcon icon='fal fa-envelope' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ data.email ?? '-' }}</dd>
-                    <CopyButton v-if="data.email" :text="data.email" />
+                    <CopyButton :text="data.email" />
                 </div>
 
                 <!-- Info: Phone -->
-                <div class="flex items-center w-full gap-x-4 px-6 py-3">
+                <div v-if="data.phone" class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
                         <span class="sr-only">Phone</span>
                         <FontAwesomeIcon icon='fal fa-phone' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ data.phone ?? '-' }}</dd>
-                    <CopyButton v-if="data.phone" :text="data.phone" />
+                    <CopyButton :text="data.phone" />
                 </div>
 
                 <!-- Info: Website -->
-                <div class="flex items-center w-full gap-x-4 px-6 py-3">
+                <div v-if="data.website" class="flex items-center w-full gap-x-4 px-6 py-3">
                     <dt class="flex items-center">
                         <span class="sr-only">Website</span>
                         <FontAwesomeIcon icon='fal fa-globe' fixed-width class='h-5 aspect-square text-gray-400' aria-hidden='true' />
                     </dt>
                     <dd class="flex items-center leading-none">{{ data.website ?? '-' }}</dd>
-                    <CopyButton v-if="data.website" :text="data.website" />
+                    <CopyButton :text="data.website" />
                 </div>
 
             </div>
