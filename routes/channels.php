c6f46cd4ca85773c7837786bcb39954d2204bb67
Raul Perusquia <raul@inikoo.com>
2023-12-08 14:35:51 +0800

    partial: live users channel 📡

--- routes/channels.php
+++ routes/channels.php
@@ -9,9 +9,17 @@ use App\Models\Auth\OrganisationUser;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('org.personal.{userID}', function (OrganisationUser $user, int $userID) {
-    return $userID===$user->id;
+    return $userID === $user->id;
 });
 
-Broadcast::channel('org.general', function (OrganisationUser $user) {
+Broadcast::channel('org.general', function (OrganisationUser $organisationUser) {
     return true;
+});
+
+Broadcast::channel('org.live.users', function (OrganisationUser $organisationUser) {
+    return [
+        'id'    => $organisationUser->id,
+        'alias' => $organisationUser->slug,
+        'name'  => $organisationUser->contact_name
+    ];
 });
