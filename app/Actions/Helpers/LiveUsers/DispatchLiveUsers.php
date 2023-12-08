c05e1b8a646848724bc815bfdb53b6c0df11f101
artha <artha@aw-advantage.com>
2023-12-08 16:46:20 +0800

    WW-115 Create route and action for Organisation Update Live User

---
+++ app/Actions/Helpers/LiveUsers/DispatchLiveUsers.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 21 Sep 2023 11:34:34 Malaysia Time, Pantai Lembeng, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Helpers\LiveUsers;
+
+use App\Events\BroadcastLiveUsers;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\AsCommand;
+
+class DispatchLiveUsers
+{
+    use AsAction;
+    use AsCommand;
+
+    public function handle(): void
+    {
+        broadcast(new BroadcastLiveUsers())->toOthers();
+    }
+
+    public function asController(ActionRequest $request): void
+    {
+        $this->handle();
+    }
+}
