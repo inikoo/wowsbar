2a5ff157ee3f7b70825f5ab8f095ae663b040ff9
aqordeon <aqordivika@yahoo.co.id>
2023-12-11 13:33:08 +0800

    fix: add log on Echo

--- resources/js/Stores/active-users.js
+++ resources/js/Stores/active-users.js
@@ -15,6 +15,7 @@ export const orgActiveUsers = defineStore("orgActiveUsers", {
 
             window.Echo.join(`org.live.users`)
             .here((rawUsers) => {
+                console.log('Who is here: ', rawUsers)
                 // on first load then store to 🍍
 
                 let users={};
@@ -26,10 +27,12 @@ export const orgActiveUsers = defineStore("orgActiveUsers", {
                 this.activeUsers = users
             })
             .joining((user) => {
+                console.log('Someone is join: ', user)
                 // If another user join from another place
                 this.activeUsers[user.id]=user;
             })
             .leaving((user) => {
+                console.log('Someone is leave: ', user)
                 // If another user leave
                // this.activeUsers.find(activeUser => activeUser.id === user.id).last_active = new Date()
                 delete this.activeUsers[user.id];
@@ -39,6 +42,7 @@ export const orgActiveUsers = defineStore("orgActiveUsers", {
                 console.log('error', error)
             })
             .listen('.changePage', (data) => {
+                console.log('Another user is change the page')
                 // Listen from another user who change the page
                 //this.activeUsers.find(activeUser => activeUser.id === data.user.id).active_page = data.data.active_page
                 //this.activeUsers.find(activeUser => activeUser.id === data.user.id).last_active = null
