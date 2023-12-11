aa179c96d43e8b444949a2f97d728637c55f0571
Raul Perusquia <raul@inikoo.com>
2023-12-11 16:34:08 +0800

    review: migrations

--- database/migrations/2023_12_08_072214_add_screenshot_id_to_email_templates_table.php
+++ database/migrations/2023_12_08_072214_add_screenshot_id_to_email_templates_table.php
@@ -1,26 +1,29 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 11 Dec 2023 15:52:39 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('email_templates', function (Blueprint $table) {
-            $table->unsignedSmallInteger('screenshot_id')->nullable()->after('compiled');
+            $table->unsignedInteger('screenshot_id')->nullable();
             $table->foreign('screenshot_id')->references('id')->on('media');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
-        //
+        Schema::table('email_templates', function (Blueprint $table) {
+            $table->dropForeign(['screenshot_id']);
+            $table->dropColumn('screenshot_id');
+        });
     }
 };
