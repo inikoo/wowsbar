aaf2bb974ca345457d3d1540b29ddd3cd9f79583
artha <artha@aw-advantage.com>
2023-12-13 11:21:23 +0800

    WW-126 Create seeder for email categories

--- database/seeders/EmailTemplateSeeder.php
+++ database/seeders/EmailTemplateSeeder.php
@@ -44,6 +44,9 @@ class EmailTemplateSeeder extends Seeder
                 );
             }
 
+            $emailTemplate->categories()->attach(
+                EmailTemplateCategory::whereIn('name', Arr::get($template, 'categories'))->pluck('id'));
+
             $imagesPath = database_path($basePath . '/images');
 
             if (File::exists($imagesPath)) {
