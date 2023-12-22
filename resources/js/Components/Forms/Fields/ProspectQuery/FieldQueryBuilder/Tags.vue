769bf2e1912b9eaf15faf346c357ada428c58fd8
YudhistaA <aryarajasa0@gmail.com>
2023-12-22 10:51:20 +0800

    negative Tags

--- resources/js/Components/Forms/Fields/ProspectQuery/FieldQueryBuilder/Tags.vue
+++ resources/js/Components/Forms/Fields/ProspectQuery/FieldQueryBuilder/Tags.vue
@@ -4,7 +4,7 @@ import Multiselect from "@vueform/multiselect"
 import axios from "axios"
 import Tag from "@/Components/Tag.vue"
 import { notify } from "@kyvg/vue3-notification"
-import { ref, onMounted, watch, reactive } from 'vue'
+import { ref, onMounted, watch, reactive, computed} from 'vue'
 
 
 const props = withDefaults(defineProps<{
@@ -29,6 +29,26 @@ const getTagsOptions = async () => {
     }
 }
 
+
+const positive = computed(() => {
+  if (!tagsOptions.value || !props.value || !props.value[props.fieldName] || !props.value[props.fieldName].negative_tag_ids) {
+    return [];
+  }
+
+  const negativeTagIds = props.value[props.fieldName].negative_tag_ids;
+  return tagsOptions.value.filter(item => !negativeTagIds.includes(item.id));
+});
+
+const negative = computed(() => {
+  if (!tagsOptions.value || !props.value || !props.value[props.fieldName] || !props.value[props.fieldName].tag_ids) {
+    return [];
+  }
+
+  const positiveTagIds = props.value[props.fieldName].tag_ids;
+  return tagsOptions.value.filter(item => !positiveTagIds.includes(item.id));
+});
+
+
 onMounted(() => {
     getTagsOptions()
 })
@@ -36,10 +56,11 @@ onMounted(() => {
 
 <template>
     <div>
-        <div>
+        <div class="mb-2">
+        <span class="font-bold text-sm block mb-1">Included Tags :</span>
             <Multiselect v-model="value[fieldName].tag_ids" mode="tags" placeholder="Select the tag" valueProp="id"
                 trackBy="name" label="name" :close-on-select="false" :searchable="true" :caret="false"
-                :options="tagsOptions" noResultsText="No one left. Type to add new one.">
+                :options="positive" noResultsText="No one left. Type to add new one.">
 
                 <template
                     #tag="{ option, handleTagRemove, disabled }: { option: tag, handleTagRemove: Function, disabled: boolean }">
@@ -50,6 +71,7 @@ onMounted(() => {
                 </template>
             </Multiselect>
         </div>
+
         <div v-if="value[fieldName].tag_ids.length > 1" class="mb-4">
             <div class="mt-1">
                 <fieldset>
@@ -65,6 +87,23 @@ onMounted(() => {
                 </fieldset>
             </div>
         </div>
+
+        <div>
+            <span class="font-bold text-sm block mb-2">Tags not included:</span>
+            <Multiselect v-model="value[fieldName].negative_tag_ids" mode="tags" placeholder="Select the tag" valueProp="id"
+                trackBy="name" label="name" :close-on-select="false" :searchable="true" :caret="false"
+                :options="negative" noResultsText="No one left. Type to add new one.">
+
+                <template
+                    #tag="{ option, handleTagRemove, disabled }: { option: tag, handleTagRemove: Function, disabled: boolean }">
+                    <div class="px-0.5 py-[3px]">
+                        <Tag :theme="option.id" :label="option.name" :closeButton="true" :stringToColor="true" size="sm"
+                            @onClose="(event) => handleTagRemove(option, event)" />
+                    </div>
+                </template>
+            </Multiselect>
+        </div>
+
     </div>
 </template>
 
