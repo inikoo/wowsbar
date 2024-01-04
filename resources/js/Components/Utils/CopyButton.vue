09c918b4ee80844829c936569b0642391b37b22c
aqordeon <aqordivika@yahoo.co.id>
2024-01-04 16:09:25 +0800

    fix: CopyButton: add icon check

--- resources/js/Components/Utils/CopyButton.vue
+++ resources/js/Components/Utils/CopyButton.vue
@@ -2,17 +2,37 @@
 import { useCopyText } from '@/Composables/useCopyText'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCopy } from '@fal'
+import { faCopy, faCheck } from '@fal'
 import { library } from '@fortawesome/fontawesome-svg-core'
-library.add(faCopy)
+import { ref } from 'vue'
+library.add(faCopy, faCheck)
 
 const props = defineProps<{
     text: string | number
 }>()
+
+const waitAfterClick = ref(false)
+
+const onClickCopy = () => {
+    waitAfterClick.value = true
+    useCopyText(props.text)
+    setTimeout(() => {
+        waitAfterClick.value = false
+    }, 1000)
+}
+
 </script>
 
 <template>
-    <div @click="useCopyText(text)" class="py-0.5 px-1 inline-flex leading-none text-gray-300 hover:text-gray-400 active:text-gray-600 cursor-pointer">
-        <FontAwesomeIcon icon='fal fa-copy' class='' aria-hidden='true' />
+    <div class="inline-flex leading-none">
+        <!-- After click copy -->
+        <div v-if="waitAfterClick" class="py-0.5 px-1">
+            <FontAwesomeIcon icon='fal fa-check' class='text-lime-500' aria-hidden='true' />
+        </div>
+
+        <!-- Copy button -->
+        <div v-else @click="onClickCopy" class="py-0.5 px-1 text-gray-300 hover:text-gray-400 active:text-gray-600 cursor-pointer">
+            <FontAwesomeIcon icon='fal fa-copy' class='' aria-hidden='true' />
+        </div>
     </div>
 </template>
