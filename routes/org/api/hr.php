ef5cbbfed2384bd16ec803d05253024c387e5059
artha <artha@aw-advantage.com>
2024-01-16 14:31:12 +0800

    feat: api mobile

--- routes/org/api/hr.php
+++ routes/org/api/hr.php
@@ -10,10 +10,14 @@ use App\Actions\HumanResources\Clocking\StoreClockingFromClockingMachine;
 use App\Actions\HumanResources\ClockingMachine\StoreClockingMachine;
 use App\Actions\HumanResources\ClockingMachine\UI\IndexClockingMachines;
 use App\Actions\HumanResources\ClockingMachine\UI\ShowClockingMachine;
+use App\Actions\HumanResources\Workplace\StoreWorkplace;
 use App\Actions\HumanResources\Workplace\UI\IndexWorkplaces;
 use App\Actions\HumanResources\Workplace\UI\ShowWorkplace;
+use App\Actions\HumanResources\Workplace\UpdateWorkplace;
 
 Route::get('workplaces', IndexWorkplaces::class)->name('workplaces.index');
+Route::post('workplaces', StoreWorkplace::class)->name('workplaces.store');
+Route::patch('workplaces/{workplace:id}', UpdateWorkplace::class)->name('workplaces.update');
 Route::get('workplaces/{workplace:id}', ShowWorkplace::class)->name('workplaces.show');
 Route::get('workplaces/{workplace:id}/clocking-machines', [IndexClockingMachines::class,'inWorkplace'])->name('workplaces.show.clocking-machines.index');
 Route::post('workplaces/{workplace:id}/clocking-machines', StoreClockingMachine::class)->name('workplaces.show.clocking-machines.store');
