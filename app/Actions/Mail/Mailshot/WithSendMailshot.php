c2dbfe37a21157b425c46edf559210445d9aefdf
artha <artha@aw-advantage.com>
2024-01-17 15:31:26 +0800

    fix: color of mailshot

--- app/Actions/Mail/Mailshot/WithSendMailshot.php
+++ app/Actions/Mail/Mailshot/WithSendMailshot.php
@@ -22,6 +22,7 @@ trait WithSendMailshot
     ) {
         $html = $emailHtmlBody;
 
+        $html = $this->processStyles($html);
 
         if (preg_match_all("/{{(.*?)}}/", $html, $matches)) {
             foreach ($matches[1] as $i => $placeholder) {
@@ -60,4 +61,23 @@ trait WithSendMailshot
         };
     }
 
+    public function processStyles($html): array|string|null
+    {
+        $html = preg_replace_callback('/<[^>]+style=["\'](.*?)["\'][^>]*>/i', function ($match) {
+            $style = $match[1];
+
+            // Find and modify color values within the style attribute
+            $style = preg_replace_callback('/color\s*:\s*([^;]+);/i', function ($colorMatch) {
+                $colorValue = $colorMatch[1];
+                $modifiedColor = $colorValue . ' !important';
+                return 'color: ' . $modifiedColor . ';';
+            }, $style);
+
+            // Update the style attribute in the HTML tag
+            return str_replace($match[1], $style, $match[0]);
+        }, $html);
+
+        // Remove <style> tags and their content
+        return preg_replace('/<style(.*?)>(.*?)<\/style>/is', '', $html);
+    }
 }
