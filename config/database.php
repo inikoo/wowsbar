8da2b432a68b18de0791835613fbf567ae0047e7
Raul Perusquia <raul@inikoo.com>
2024-01-18 09:56:20 +0800

    feat: limit call to CheckPendingSenderEmailVerifications

--- config/database.php
+++ config/database.php
@@ -118,7 +118,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'wowsbar'), '_').'_database_'),
+            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'wowsbar'), '_').'_'.env('APP_ENV').'_database_'),
         ],
 
         'default' => [
