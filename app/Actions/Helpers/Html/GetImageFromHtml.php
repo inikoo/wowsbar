8d679c90cef047386d1ccc4bd5d0ad0625194cec
Raul Perusquia <raul@inikoo.com>
2024-02-16 01:59:12 -0600

    feat: work in stake screenshots 📸

--- app/Actions/Helpers/Html/GetImageFromHtml.php
+++ app/Actions/Helpers/Html/GetImageFromHtml.php
@@ -7,7 +7,6 @@
 
 namespace App\Actions\Helpers\Html;
 
-use Illuminate\Support\Facades\File;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
 use Spatie\Browsershot\Browsershot;
@@ -17,32 +16,28 @@ class GetImageFromHtml
     use AsAction;
     use WithAttributes;
 
-    public function handle($html, $filename): array
+    public function handle($html)
     {
-        $path = storage_path('app/tmp/screenshots/');
 
-        if (!File::exists($path)) {
-            File::makeDirectory($path, 0755, true);
-        }
-
-        $filename = $filename.'.jpg';
-
-        $shot = Browsershot::html($html)
+        $browserShot = Browsershot::html($html)
             ->setIncludePath('$PATH:/usr/bin')
             ->fullPage()
             ->setOption('newHeadless', true)
             ->base64Screenshot();
 
-        $image = str_replace('data:image/png;base64,', '', $shot);
-        $image = str_replace(' ', '+', $image);
+        $browserShot = str_replace('data:image/png;base64,', '', $browserShot);
+        $browserShot = str_replace(' ', '+', $browserShot);
 
-        File::put($path.$filename, base64_decode($image));
 
-        return [
-            'path'     => $path,
-            'filename' => $filename,
-            'fullPath' => $path.$filename
-        ];
+        $temp = tmpfile();
+
+
+
+        fwrite($temp, base64_decode($browserShot));
+
+        return $temp;
+
+
     }
 
 }
--- app/Actions/Mail/EmailTemplate/SetEmailTemplateScreenshot.php
+++ app/Actions/Mail/EmailTemplate/SetEmailTemplateScreenshot.php
@@ -11,8 +11,10 @@ use App\Actions\Helpers\Html\GetImageFromHtml;
 use App\Actions\Traits\WithActionUpdate;
 use App\Actions\Traits\WIthSaveUploadedImage;
 use App\Models\Mail\EmailTemplate;
-use Illuminate\Support\Facades\File;
+use Exception;
+use Illuminate\Console\Command;
 use Lorisleiva\Actions\Concerns\AsAction;
+use Str;
 
 class SetEmailTemplateScreenshot
 {
@@ -20,29 +22,63 @@ class SetEmailTemplateScreenshot
     use WithActionUpdate;
     use WIthSaveUploadedImage;
 
+
     /**
      * @throws \Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist
      * @throws \Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig
      */
     public function handle(EmailTemplate $emailTemplate): EmailTemplate
     {
-        $imagesPath = GetImageFromHtml::run(
-            $emailTemplate->compiled['html']['html'],
-            $emailTemplate->slug
+
+        $tmpFile = GetImageFromHtml::run(
+            $emailTemplate->compiled['html']['html']
         );
 
-        if (File::exists($imagesPath['path'])) {
-            foreach (File::files($imagesPath['path']) as $image) {
-                return $this->saveUploadedImage(
-                    model: $emailTemplate,
-                    collection: 'screenshot',
-                    field: 'screenshot_id',
-                    imagePath: $image->getPathname(),
-                    originalFilename: $image->getFilename()
-                );
-            }
-        }
+
+        $this->saveUploadedImage(
+            model: $emailTemplate,
+            collection: 'screenshot',
+            field: 'screenshot_id',
+            imagePath: stream_get_meta_data($tmpFile)['uri'],
+            originalFilename: Str::kebab($emailTemplate->name) . '.jpg'
+        );
+
+        fclose($tmpFile);
 
         return $emailTemplate;
     }
+
+    public string $commandSignature = 'email-template:screenshot {email_template_id?}';
+
+
+    /**
+     * @throws \Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist
+     * @throws \Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig
+     */
+    public function asCommand(Command $command): int
+    {
+        if ($command->argument('email_template_id')) {
+            try {
+                $emailTemplate = EmailTemplate::find($command->argument('email_template_id'));
+                if ($emailTemplate) {
+                    $this->handle($emailTemplate);
+                    $command->info('Screenshot saved');
+
+                    return 0;
+                }
+            } catch (Exception $e) {
+                $command->error($e->getMessage());
+
+                return 1;
+            }
+        } else {
+            foreach (EmailTemplate::where('is_seeded', false)->get() as $emailTemplate) {
+                $this->handle($emailTemplate);
+            }
+        }
+
+
+        return 0;
+    }
+
 }
