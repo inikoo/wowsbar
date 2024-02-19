c24867cc7dc2a1a08a687f285e73eab88b4093f4
aqordeon <aqordivika@yahoo.co.id>
2024-02-19 09:47:35 +0800

    fix: Banner: word on Corner text was broken

--- resources/js/Composables/useRemoveHttps.ts
+++ resources/js/Composables/useRemoveHttps.ts
@@ -1,5 +1,5 @@
 // To remove https:// or http:// from a string
-export const useRemoveHttps = (text: string) => {
+export const useRemoveHttps = (text: string | undefined) => {
     if(!text) return ''
 
     return text.toString().replace(/^https?:\/\//g, '')
