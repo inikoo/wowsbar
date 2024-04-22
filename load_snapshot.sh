22434c88d218e4400ee063a3caf88ffbe3aaa1bc
Raul Perusquia <raul@inikoo.com>
2024-04-22 15:27:24 +0800

    load snapshot script

--- load_snapshot.sh
+++ load_snapshot.sh
@@ -1,2 +1,2 @@
-pg_restore -j 15  -U aiku -c -d wowsbar  "devops/devel/snapshots/$1.dump"
+pg_restore --no-owner --no-acl --clean --if-exists -j 15 -U ${USER} -c -d wowsbar  "devops/devel/snapshots/$1.dump"
 echo "$1"
