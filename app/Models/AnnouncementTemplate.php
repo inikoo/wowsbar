856d821aa97778d15d1747d8034be3b057cda34e
Artha <manager.arthachan@gmail.com>
2024-10-25 14:51:26 +0800

    fix: relations

--- app/Models/AnnouncementTemplate.php
+++ app/Models/AnnouncementTemplate.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Media\Media;
 use App\Models\Traits\HasImages;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\MediaLibrary\HasMedia;
 
 class AnnouncementTemplate extends Model implements HasMedia
@@ -13,4 +15,9 @@ class AnnouncementTemplate extends Model implements HasMedia
     use HasImages;
 
     protected $guarded = [];
+
+    public function screenshot(): BelongsTo
+    {
+        return $this->belongsTo(Media::class, 'screenshot_id');
+    }
 }
