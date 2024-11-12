b45f744469c03b9fc994fdbc2f751744fc13fc89
Raul Perusquia <raul@inikoo.com>
2024-11-12 09:37:28 +0800

    mv code -> template_code in announcements db

---
+++ database/migrations/2024_11_11_084957_remove_title_from_announcements.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::table('announcements', function (Blueprint $table) {
+            if (Schema::hasColumn('announcements', 'code')) {
+                $table->dropColumn('code');
+            }
+            $table->string('template_code')->index();
+
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('announcements', function($table) {
+            $table->string('code')->nullable();
+            $table->dropColumn('template_code');
+        });
+
+    }
+};
