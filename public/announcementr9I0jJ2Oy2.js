185103c5a94c557a3723092becc24ee66b9ae379
aqordeon <aqordivika@yahoo.co.id>
2024-11-27 13:47:13 +0800

    fix: Announcement: small restructure

--- public/announcementr9I0jJ2Oy2.js
+++ public/announcementr9I0jJ2Oy2.js
@@ -1,16 +1,9 @@
 
-const scriptUrl = new URL(document.currentScript.src);
-
-// Extract the `ulid` from the query parameters
-const ulid = scriptUrl.searchParams.get('ulid');
-const deliveryUrl = scriptUrl.searchParams.get('delivery');
-
-// Use the ulid as needed
-console.log('ULID:', ulid);
 
 
-function iframeStyle(iframeElement) {
-    iframeElement.id = `iframe-wowsbar-${ulid}`;
+
+function iframeStyle(iframeElement, ulidAnnouncement) {
+    iframeElement.id = `iframe-wowsbar-${ulidAnnouncement}`;
     iframeElement.setAttribute('allowTransparency', 'true');
     iframeElement.style.height = '0px'
     iframeElement.style.width = '100%';
@@ -26,12 +19,18 @@ function iframeStyle(iframeElement) {
 
 
 async function fetchAnnouncementData() {
+    const scriptUrl = new URL(document.currentScript.src);
+
+    // Extract the `ulid` from the query parameters
+    const ulid = scriptUrl.searchParams.get('ulid');
+    const deliveryUrl = scriptUrl.searchParams.get('delivery');
+    console.log('ulid:', ulid, 'deliveryUrl:', deliveryUrl)
+    
+    const domain = window.location.hostname
+    const path = window.location.pathname
+    console.log('domain:', domain, 'path:', path)
+
     if (ulid) {
-        const domain = window.location.hostname
-        const path = window.location.pathname
-        // console.log('domain', domainName)
-        console.log('deliveryUrl', deliveryUrl)
-        // console.log('fetchAnnouncementData')
         try {
             // Fetch: Announcement JSON
             const announcementData = await fetch(`ar_web_wowsbar_announcement.php?url_KHj321Tu=${deliveryUrl}/announcement/${ulid}`, {
@@ -41,59 +40,58 @@ async function fetchAnnouncementData() {
                 }
             })
                 .then(async response => {
-                    console.log('response announce', await response.text())
                     const xxx = await response.text();
-                    console.log('resresrespon', xxx);
+                    console.log('ar_web_wowsbar:', xxx);
 
                     if (!response.ok) {
-                        throw new Error('Network response was not ok ' + response.statusText);
+                        throw new Error('Network response was not ok ---- ' + response.statusText);
                     }
 
-                    document.querySelector('#wowsbar_announcement').innerHTML = xxx;
+                    // document.querySelector('#wowsbar_announcement').innerHTML = xxx;
 
                     return xxx;
                 });
 
             console.log('======== new Announctment data', announcementData);
-            // document.querySelector('#wowsbar_announcement').innerHTML = announcementData;
+            document.querySelector('#wowsbar_announcement').innerHTML = announcementData;
             
-            // const containerStyle = propertiesToHTMLStyle(announcementData.container_properties)
-            // console.log('announcementData', containerStyle);
+            const containerStyle = propertiesToHTMLStyle(announcementData.container_properties)
+            console.log('announcementData', containerStyle);
 
-            // // createElementAfterBody();
+            // createElementAfterBody();
 
-            // const iframe = document.createElement('iframe');
-            // iframe.src = `https://delivery-staging.wowsbar.com/announcement/${ulid}`;
-            // iframeStyle(iframe)
+            const iframe = document.createElement('iframe');
+            iframe.src = `https://delivery-staging.wowsbar.com/announcement/${ulid}`;
+            iframeStyle(iframe, ulid)
 
-            // // console.log('zzzz', iframe?.contentWindow)
-            // // function adjustIframeHeight() {
-            // //     iframe.style.height = iframe.contentWindow.document.body.clientHeight + 'px';
-            // //     console.log('ffff', iframe.style.height)
-            // // }
-            
-            // let iframeHeight
-            // let iframeTop
-
-            // // Listening event from component (inside iframe)
-            // window.addEventListener('message', function(event) {
-            //     console.log('received message', event.data)
-
-            //     // Emit from each component (AnnouncementInformation1)
-            //     if(event.data === 'close_button_click') {
-            //         console.log('Close button clicked')
-            //         iframe.style.top = `-${parseInt(containerStyle.height, 10) + parseInt(containerStyle.height, 10)}px`
-            //     }
-            // });
-
-            // // Set style for iframe
-            // for (const [key, value] of Object.entries(containerStyle)) {
-            //     iframe.style[key] = value;
+            // console.log('zzzz', iframe?.contentWindow)
+            // function adjustIframeHeight() {
+            //     iframe.style.height = iframe.contentWindow.document.body.clientHeight + 'px';
+            //     console.log('ffff', iframe.style.height)
             // }
             
-            // // Insert iframe to first child of <body>
-            // const body = document.body;
-            // body.insertBefore(iframe, body.firstChild);
+            let iframeHeight
+            let iframeTop
+
+            // Listening event from component (inside iframe)
+            window.addEventListener('message', function(event) {
+                console.log('received message', event.data)
+
+                // Emit from each component (AnnouncementInformation1)
+                if(event.data === 'close_button_click') {
+                    console.log('Close button clicked')
+                    iframe.style.top = `-${parseInt(containerStyle.height, 10) + parseInt(containerStyle.height, 10)}px`
+                }
+            });
+
+            // Set style for iframe
+            for (const [key, value] of Object.entries(containerStyle)) {
+                iframe.style[key] = value;
+            }
+            
+            // Insert iframe to first child of <body>
+            const body = document.body;
+            body.insertBefore(iframe, body.firstChild);
 
         } catch (error) {
             console.error('Someting went wrong:', error);
